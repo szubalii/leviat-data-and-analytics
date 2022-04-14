af35410480755e7c848e713bbd205fe9f7f162da
Michiel Pors <you@example.com>
2022-04-14 14:03:32 +0200

    correct field reference

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Tables/layer_activity.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Tables/layer_activity.sql
@@ -3,5 +3,5 @@ CREATE TABLE [dbo].[layer_activity] (
   [activity_id] BIGINT NOT NULL,
   PRIMARY KEY CLUSTERED ([layer_id], [activity_id] ASC),
   CONSTRAINT [FK_layer_activity_layer] FOREIGN KEY ([layer_id]) REFERENCES [dbo].[layer] ([layer_id]),
-  CONSTRAINT [FK_layer_activity_activity] FOREIGN KEY ([activity_id]) REFERENCES [dbo].[activity] ([activity_id])
+  CONSTRAINT [FK_layer_activity_activity] FOREIGN KEY ([activity_id]) REFERENCES [dbo].[batch_activity] ([activity_id])
 );
