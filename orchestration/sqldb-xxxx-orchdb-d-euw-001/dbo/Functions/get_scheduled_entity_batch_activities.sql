beefbcc9f2d082bb0d1338c81fa19679507567aa
Michiel Pors <you@example.com>
2022-04-26 09:44:02 +0200

    update get_scheduled_entity_batch_activities

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/get_scheduled_entity_batch_activities.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/get_scheduled_entity_batch_activities.sql
@@ -5,46 +5,50 @@ CREATE FUNCTION [dbo].[get_scheduled_entity_batch_activities](
 )
 RETURNS TABLE AS RETURN
 
-    WITH pipelines AS (
-        select
+    WITH latest_pipeline AS (
+        select top 1
             b.entity_id,
-            pl.parent_run_id,
-            MIN(pl.start_date_time) as parent_run_start_time
+            pl.run_id,
+            MIN(pl.start_date_time) as run_start_time
         from
             batch b
         LEFT JOIN [dbo].[pipeline_log] pl
             ON pl.[run_id] = b.[run_id]
         WHERE
-            CONVERT(date, b.start_date_time) = @date
+            CONVERT(date, pl.start_date_time) = @date
             -- AND
             -- b.entity_id = @entity_id
         group by
             b.entity_id,
-            pl.parent_run_id
-    ),
-    latest_pipeline_start_date_time AS (
-        select
-            entity_id,
-            max(parent_run_start_time) as latest_parent_run_start_time
-        from pipelines
-        group by entity_id
-    ),
-    latest_pipeline AS (
-        select
-            p.entity_id,
-            p.parent_run_id,
-            lp.latest_parent_run_start_time
-        from pipelines p
-        left join latest_pipeline_start_date_time lp
-            on lp.entity_id = p.entity_id and
-                lp.latest_parent_run_start_time = p.parent_run_start_time
-        where latest_parent_run_start_time is not null
-    ),
-    latest_batch AS (
+            pl.run_id
+        order by run_start_time desc
+    )
+    -- ,
+    -- latest_pipeline AS (
+    --     select
+    --         pl.run_id
+    --     from
+    --         latest_pipeline_start_dtm pl
+    --     LEFT JOIN latest_pipeline_start_dtm lpsd
+    --         ON lpsd.[run_start_time] = pl.[run_id]
+    --     WHERE
+    --         CONVERT(date, pl.start_date_time) = @date
+    --         -- AND
+    --         -- b.entity_id = @entity_id
+    --     group by
+    --         b.entity_id--,
+    --         -- pl.run_id
+    --     -- HAVING
+    --     --     MAX(pl.start_date_time)
+    -- )
+    , latest_batch AS (
 
         SELECT
-            pl.parent_run_id,
-            pl.start_date_time AS pipeline_start_date_time,
+            -- pl.parent_run_id,
+            -- pl.start_date_time AS pipeline_start_date_time,
+            b.batch_id,
+            lp.run_id,
+            lp.run_start_time,
             e.entity_name,
             e.entity_id,
             e.layer_id,
@@ -54,7 +58,8 @@ RETURNS TABLE AS RETURN
             ba.activity_order,
             bs.status_nk,
             b.directory_path,
-            b.file_name
+            b.file_name,
+            b.output
         FROM
             [dbo].[batch] b
         LEFT JOIN [dbo].[entity] e
@@ -69,11 +74,10 @@ RETURNS TABLE AS RETURN
             ON bs.[status_id] = b.[status_id]
         LEFT JOIN [dbo].[batch_activity] ba
             ON ba.[activity_id] = b.[activity_id]
-        LEFT JOIN [dbo].[pipeline_log] pl
-            ON pl.[run_id] = b.[run_id]
-        LEFT JOIN latest_pipeline lp
-            ON lp.parent_run_id = pl.parent_run_id and
-            lp.entity_id = b.entity_id
+        -- LEFT JOIN [dbo].[pipeline_log] pl
+        --     ON pl.[run_id] = b.[run_id]
+        INNER JOIN latest_pipeline lp
+            ON lp.run_id = b.run_id
 
 
         WHERE
@@ -82,8 +86,8 @@ RETURNS TABLE AS RETURN
             -- b.entity_id = @entity_id
             AND
             e.update_mode = 'Full'
-            and
-            lp.parent_run_id is not null
+            -- and
+            -- lp.parent_run_id is not null
         -- ORDER BY
         --     entity_name asc, start_date_time desc
         -- select * from batch
@@ -117,74 +121,171 @@ RETURNS TABLE AS RETURN
 
     -- DECLARE
     --     @date DATE = '2022/04/11';
+    , scheduled_entity_batch_activities as (
+        select
+            e.entity_id,
+            e.entity_name,
+            e.layer_nk,
+            e.client_field,
+            e.extraction_type,
+            e.pk_field_names,
+            e.[axbi_database_name],
+            e.[axbi_schema_name],
+            e.[base_table_name],
+            e.[axbi_date_field_name],
+            e.adls_container_name,
+            e.adls_directory_path_In,
+            e.adls_directory_path_Out,
+            e.[base_schema_name],
+            e.[base_sproc_name],
+            e.file_name,
+            ba.activity_nk,
+            ba.activity_order,
+            lb.batch_id,
+            lb.status_nk,
+            case
+                when lb.output is null then '{}'
+                else lb.output
+            end as output,
+            -- concat('["', string_agg(ba.activity_nk,'","')
+            --     within group (order by ba.activity_order asc), '"]')
+            --     as required_activities,
+            case when status_nk = 'Succeeded' then 0 else 1 end as [isRequired]
+        from
+            get_scheduled_entities(0, @date) e
+        left join
+            layer l
+            ON
+                l.layer_nk = e.layer_nk
+        left join
+            layer_activity la
+            on
+                la.layer_id = l.layer_id
+        left join
+            batch_activity ba
+            on
+                ba.activity_id = la.activity_id
+        left join
+            latest_batch lb
+            on lb.entity_id = e.entity_id and
+                lb.activity_nk = ba.activity_nk
+
+        WHERE
+            e.layer_nk IN ('S4H', 'AXBI', 'USA')
+            AND
+            e.update_mode = 'Full'
+            -- and
+            -- CONVERT(date, b.start_date_time) = @date
+        -- group by
+        --     e.entity_id,
+        --     e.entity_name,
+        --     e.layer_nk,
+        --     e.client_field,
+        --     e.extraction_type,
+        --     e.pk_field_names,
+        --     e.[axbi_database_name],
+        --     e.[axbi_schema_name],
+        --     e.[base_table_name],
+        --     e.[axbi_date_field_name],
+        --     e.adls_container_name,
+        --     e.adls_directory_path_In,
+        --     e.adls_directory_path_Out,
+        --     e.[base_schema_name],
+        --     e.[base_sproc_name],
+        --     e.file_name
+    )
+    , activities as (
+        select
+            entity_id,
+            entity_name,
+            layer_nk,
+            client_field,
+            extraction_type,
+            pk_field_names,
+            [axbi_database_name],
+            [axbi_schema_name],
+            [base_table_name],
+            [axbi_date_field_name],
+            adls_container_name,
+            adls_directory_path_In,
+            adls_directory_path_Out,
+            [base_schema_name],
+            [base_sproc_name],
+            file_name,
+            -- ba.activity_nk,
+            -- ba.activity_order,
+            -- lb.batch_id,
+            -- lb.status_nk,
+            -- lb.output,
+            case
+                when isRequired = 1 then
+                    concat('["', string_agg(activity_nk, '","')
+                    within group (order by activity_order asc), '"]')
+                else null
+            end as required_activities,
+            case
+                when isRequired = 0 then
+                    concat('{', string_agg(concat('"', activity_nk, '": {"batch_id":"', batch_id, '", "output":', [output], '}'), ',')
+                    within group (order by activity_order asc), '}')
+                else null
+            end as skipped_activities
+        from scheduled_entity_batch_activities
+        group by
+            entity_id,
+            entity_name,
+            layer_nk,
+            client_field,
+            extraction_type,
+            pk_field_names,
+            [axbi_database_name],
+            [axbi_schema_name],
+            [base_table_name],
+            [axbi_date_field_name],
+            adls_container_name,
+            adls_directory_path_In,
+            adls_directory_path_Out,
+            [base_schema_name],
+            [base_sproc_name],
+            file_name,
+            isRequired
+    )
 
     select
-        e.entity_id,
-        e.entity_name,
-        e.layer_nk,
-        e.client_field,
-        e.extraction_type,
-        e.pk_field_names,
-        e.[axbi_database_name],
-        e.[axbi_schema_name],
-        e.[base_table_name],
-        e.[axbi_date_field_name],
-        e.adls_container_name,
-        e.adls_directory_path_In,
-        e.adls_directory_path_Out,
-        e.[base_schema_name],
-        e.[base_sproc_name],
-        e.file_name,
-        concat('["', string_agg(ba.activity_nk,'","')
-            within group (order by ba.activity_order asc), '"]')
-            as required_activities,
-        '{}' as skipped_activities
-    from
-        get_scheduled_entities(0, @date) e
-    left join
-        layer l
-        ON
-            l.layer_nk = e.layer_nk
-    left join
-        layer_activity la
-        on
-            la.layer_id = l.layer_id
-    left join
-        batch_activity ba
-        on
-            ba.activity_id = la.activity_id
-    left join
-        latest_batch lb
-        on lb.entity_id = e.entity_id and
-            lb.activity_nk = ba.activity_nk
-
-    WHERE
-        e.layer_nk IN ('S4H', 'AXBI', 'USA')
-        AND
-        e.update_mode = 'Full'
-        -- and
-        -- CONVERT(date, b.start_date_time) = @date
+        entity_id,
+        entity_name,
+        layer_nk,
+        client_field,
+        extraction_type,
+        pk_field_names,
+        [axbi_database_name],
+        [axbi_schema_name],
+        [base_table_name],
+        [axbi_date_field_name],
+        adls_container_name,
+        adls_directory_path_In,
+        adls_directory_path_Out,
+        [base_schema_name],
+        [base_sproc_name],
+        file_name,
+        MAX(required_activities) as required_activities,
+        MAX(skipped_activities) as skipped_activities
+    from activities
     group by
-        e.entity_id,
-        e.entity_name,
-        e.layer_nk,
-        e.client_field,
-        e.extraction_type,
-        e.pk_field_names,
-        e.[axbi_database_name],
-        e.[axbi_schema_name],
-        e.[base_table_name],
-        e.[axbi_date_field_name],
-        e.adls_container_name,
-        e.adls_directory_path_In,
-        e.adls_directory_path_Out,
-        e.[base_schema_name],
-        e.[base_sproc_name],
-        e.file_name
-    --order by entity_id--, ba.activity_order
-        -- and
-        -- CONVERT(date, b.start_date_time) = @date
-    -- order by entity_id, activity_order
-
--- select * from get_scheduled_entities(0, '2022/04/11')
+        entity_id,
+        entity_name,
+        layer_nk,
+        client_field,
+        extraction_type,
+        pk_field_names,
+        [axbi_database_name],
+        [axbi_schema_name],
+        [base_table_name],
+        [axbi_date_field_name],
+        adls_container_name,
+        adls_directory_path_In,
+        adls_directory_path_Out,
+        [base_schema_name],
+        [base_sproc_name],
+        file_name
+    order by entity_id
 GO
