1706a3f4fffac59720f1bee84a6ebb57839a0f9d
Michiel Pors <you@example.com>
2022-05-03 13:44:43 +0200

    update to get_scheduled_delta_entity_batch_activities

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/get_scheduled_delta_entity_batch_activities.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/get_scheduled_delta_entity_batch_activities.sql
@@ -13,11 +13,13 @@ RETURNS TABLE AS RETURN
             e.update_mode,
             b.run_id,
             b.batch_id,
+            b.activity_id,
             ba.activity_nk,
             ba.activity_order,
             b.start_date_time,
             bs.status_nk,
-            b.file_name
+            b.file_name,
+            b.output
             -- MAX(b.start_date_time) as start_date_time
         FROM
             batch b
@@ -43,11 +45,74 @@ RETURNS TABLE AS RETURN
         WHERE
             -- CONVERT(date, b.start_date_time) = @date
             -- and
-            b.entity_id = 387--@entity_name
+            b.entity_id = 386--@entity_name
         -- GROUP BY
         --     b.entity_id,
         --     b.activity_id
     )
+    -- get list of files and join with corresponding layer activity
+    , file_batch_activities as (
+        select
+            dfba.entity_id,
+            dfba.entity_name,
+            dfba.layer_id,
+            dfba.update_mode,
+            -- dfba.run_id,
+            -- dfba.batch_id,
+            -- dfba.activity_nk,
+            -- dfba.activity_order,
+            -- dfba.start_date_time,
+            -- dfba.status_nk,
+            dfba.file_name,
+            la.activity_id
+        from delta_file_batch_activities dfba
+        left join layer_activity la
+        on la.layer_id = dfba.layer_id
+        group by
+            dfba.entity_id,
+            dfba.entity_name,
+            dfba.layer_id,
+            dfba.update_mode,
+            -- dfba.run_id,
+            -- dfba.batch_id,
+            -- dfba.activity_nk,
+            -- dfba.activity_order,
+            -- dfba.start_date_time,
+            -- dfba.status_nk,
+            dfba.file_name,
+            la.activity_id
+        -- order by file_name, activity_id
+    )
+    , potential_batch_activities as (
+        SELECT
+            fba.entity_id,
+            fba.entity_name,
+            fba.layer_id,
+            fba.update_mode,
+            fba.file_name,
+            fba.activity_id,
+            dfba.run_id,
+            dfba.batch_id,
+            ba.activity_nk,
+            ba.activity_order,
+            dfba.start_date_time,
+            dfba.status_nk,
+            dfba.output
+        from file_batch_activities fba
+        left JOIN
+            delta_file_batch_activities dfba
+            on
+                dfba.entity_id = fba.entity_id
+                and
+                dfba.activity_id = fba.activity_id
+                and
+                dfba.file_name = fba.file_name
+        left JOIN
+            batch_activity ba
+            on ba.activity_id = fba.activity_id
+        -- order by fba.file_name, activity_order
+    )
+
 
 
     -- select * from latest_batch
@@ -95,30 +160,34 @@ RETURNS TABLE AS RETURN
             e.adls_directory_path_Out,
             e.[base_schema_name],
             e.[base_sproc_name],
-            dfba.file_name,
-            ba.activity_nk,
-            ba.activity_order,
-            dfba.batch_id,
-            dfba.status_nk,
+            pba.file_name,
+            pba.activity_nk,
+            pba.activity_order,
+            pba.batch_id,
+            pba.status_nk,
+            start_date_time,
+            case
+                when pba.output is null then '{}'
+                else pba.output
+            end as output,
             case when status_nk = 'Succeeded' then 0 else 1 end as [isRequired]
         from
             get_scheduled_entities(0, @date) e
+        -- left join
+        --     layer l
+        --     ON
+        --         l.layer_nk = e.layer_nk
+        -- left join
+        --     layer_activity la
+        --     on
+        --         la.layer_id = l.layer_id
+        -- left join
+        --     batch_activity ba
+        --     on
+        --         ba.activity_id = la.activity_id
         left join
-            layer l
-            ON
-                l.layer_nk = e.layer_nk
-        left join
-            layer_activity la
-            on
-                la.layer_id = l.layer_id
-        left join
-            batch_activity ba
-            on
-                ba.activity_id = la.activity_id
-        left join
-            delta_file_batch_activities dfba
-            on dfba.entity_id = e.entity_id and
-                dfba.activity_nk = ba.activity_nk
+            potential_batch_activities pba
+            on pba.entity_id = e.entity_id
 
         WHERE
             e.layer_nk IN ('S4H', 'AXBI', 'USA')
@@ -127,16 +196,17 @@ RETURNS TABLE AS RETURN
             )
             AND
             (
-                (ba.activity_nk = 'TestDuplicates' AND e.pk_field_names IS NOT NULL)
+                (pba.activity_nk = 'TestDuplicates' AND e.pk_field_names IS NOT NULL)
                 OR
-                ba.activity_nk != 'TestDuplicates'
+                pba.activity_nk != 'TestDuplicates'
             )
             AND
             (
-                (ba.activity_nk = 'ProcessBase' AND e.base_sproc_name IS NOT NULL)
+                (pba.activity_nk = 'ProcessBase' AND e.base_sproc_name IS NOT NULL)
                 OR
-                ba.activity_nk != 'ProcessBase'
+                pba.activity_nk != 'ProcessBase'
             )
+            and e.entity_id = 386
         -- order by 
     )
     , activities as (
@@ -179,6 +249,8 @@ RETURNS TABLE AS RETURN
                             activity_nk,
                             '": {"batch_id":"',
                             batch_id,
+                            '", "output":',
+                            output,
                             '}'
                         ),
                         ','
