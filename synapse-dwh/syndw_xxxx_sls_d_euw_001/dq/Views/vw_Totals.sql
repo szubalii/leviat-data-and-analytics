d991cf500da737bc24bdf6360166b1714ad3a56b
Ksenia Puchkova <kpuchkova@crh.com>
2022-06-28 11:19:21 +0300

    moved totals union from sp to view

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dq/Views/vw_Totals.sql
@@ -0,0 +1,57 @@
+﻿CREATE VIEW [dq].[vw_Totals]
+	AS  
+
+    SELECT
+        [RuleID]
+        , [Product] 
+    FROM
+        [dq].[vw_Product_1_2]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product]  
+    FROM
+        [dq].[vw_Product_1_8]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product]    
+    FROM
+        [dq].[vw_Product_1_16]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product]   
+    FROM
+        [dq].[vw_Product_1_17]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product]    
+    FROM
+        [dq].[vw_Product_1_18]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product] 
+    FROM
+        [dq].[vw_Product_1_19]
+
+    UNION ALL
+
+    SELECT
+        [RuleID]
+        , [Product]   
+    FROM
+        [dq].[vw_Product_1_20]
+ 
