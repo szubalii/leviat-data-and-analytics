e9496d4ac0f637bcc58464fbb9798025f287a56e
MINSK\Natallia_Baranava <natallia.baranava@refinitiv.com>
2022-06-30 15:01:20 +0300

    add views to load dq tables, delete sproc

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dq/Views/vw_ProductTotal.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dq].[vw_ProductTotal] AS
+SELECT 
+    (SELECT COUNT([Product]) FROM [base_s4h_cax].[I_Product]) AS [ProductTotals]
+    , COUNT(DISTINCT t.[Product]) AS [ErrorTotals]
+FROM 
+	[dq].[vw_RuleProduct] t
