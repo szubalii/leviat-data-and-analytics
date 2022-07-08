03738c9bdb1e1a4bb00e9225f89c6c921b64582b
Naresh Sriram <naresh_sriram@epam.com>
2022-07-08 09:28:13 +0200

    Checking in ProductsalesDelivery review for review

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_ProductSalesDelivery.sql
@@ -0,0 +1,13 @@
+CREATE VIEW [dm_dq].[vw_dim_ProductSalesDelivery] AS 
+
+SELECT [sk_ProductSalesDelivery]
+      ,[ProductID]
+      ,[SalesOrganizationID]
+      ,[DistributionChannelID]
+      ,[Product]
+      ,[SalesOrganization]
+      ,[DistributionChannel]
+      ,[AccountDetnProductGroup]
+      ,[CashDiscountIsDeductible]
+	  ,[IsMarkedForDeletion]
+FROM [edw].[dim_ProductSalesDelivery]
