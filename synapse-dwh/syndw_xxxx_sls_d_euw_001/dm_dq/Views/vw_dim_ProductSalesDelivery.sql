2a4c8a4790353daf926aef22979a5b303302dbef
Naresh Sriram <naresh_sriram@epam.com>
2022-07-08 11:41:45 +0200

    Fixed dormatting

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_ProductSalesDelivery.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_ProductSalesDelivery.sql
@@ -1,13 +1,15 @@
 CREATE VIEW [dm_dq].[vw_dim_ProductSalesDelivery] AS 
 
-SELECT [sk_ProductSalesDelivery]
-      ,[ProductID]
-      ,[SalesOrganizationID]
-      ,[DistributionChannelID]
-      ,[Product]
-      ,[SalesOrganization]
-      ,[DistributionChannel]
-      ,[AccountDetnProductGroup]
-      ,[CashDiscountIsDeductible]
-	  ,[IsMarkedForDeletion]
-FROM [edw].[dim_ProductSalesDelivery]
+SELECT 
+     [sk_ProductSalesDelivery]
+    ,[ProductID]
+    ,[SalesOrganizationID]
+    ,[DistributionChannelID]
+    ,[Product]
+    ,[SalesOrganization]
+    ,[DistributionChannel]
+    ,[AccountDetnProductGroup]
+    ,[CashDiscountIsDeductible]
+    ,[IsMarkedForDeletion]
+FROM 
+    [edw].[dim_ProductSalesDelivery]
