3a95ab87bf6adeea0c6e7505b7a822ddf3183507
Barannik Raisa <barannik.rb@pg.com>
2022-07-12 12:10:29 +0300

    US_1143 new table for synapse from TX_HALFEN_2_DWH_PROD

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_tx_halfen_2_dwh/Tables/DIM_ADUENUMTRANSLATIONS.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [base_tx_halfen_2_dwh].[DIM_ADUENUMTRANSLATIONS]
+(
+	[DW_Id]				BIGINT IDENTITY(1,1) NOT NULL,
+	[ENUMID] 			BIGINT NULL,
+	[ENUMVALUE] 		BIGINT NULL,
+	[ENUMNAME] 			NVARCHAR(50) NULL,
+	[ENUMLABELEN]		NVARCHAR(1999) NULL,
+	[DW_Batch]			BIGINT NULL,
+	[DW_SourceCode] 	VARCHAR(15) NOT NULL,
+	[DW_TimeStamp]		DATETIME NOT NULL,
+	[t_applicationId]   VARCHAR    (32)  NULL,
+    [t_jobId]           VARCHAR    (36)  NULL,
+    [t_jobDtm]          DATETIME,
+    [t_jobBy]           NVARCHAR  (128)  NULL,
+    [t_extractionDtm]   DATETIME,
+    [t_filePath]        NVARCHAR (1024)  NULL,
+    CONSTRAINT [PK_DIM_ADUENUMTRANSLATIONS] PRIMARY KEY NONCLUSTERED (
+        [DW_Id] ASC
+    ) NOT ENFORCED
+)
+WITH (HEAP, DISTRIBUTION = ROUND_ROBIN);
---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_tx_halfen_2_dwh/Tables/DIM_INVENTDIM.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [base_tx_halfen_2_dwh].[DIM_INVENTDIM]
+(
+	[DW_Id] 			BIGINT IDENTITY(1,1) NOT NULL,
+	[INVENTDIMID]		NVARCHAR(20) NULL,
+	[INVENTSITEID] 		NVARCHAR(10) NULL,
+	[INVENTLOCATIONID] 	NVARCHAR(10) NULL,
+	[CONFIGID] 			NVARCHAR(24) NULL,
+	[DW_Batch] 			BIGINT NULL,
+	[DW_SourceCode] 	VARCHAR(15) NOT NULL,
+	[DW_TimeStamp] 		DATETIME NOT NULL,
+	[t_applicationId]   VARCHAR    (32)  NULL,
+    [t_jobId]           VARCHAR    (36)  NULL,
+    [t_jobDtm]          DATETIME,
+    [t_jobBy]           NVARCHAR  (128)  NULL,
+    [t_extractionDtm]   DATETIME,
+    [t_filePath]        NVARCHAR (1024)  NULL,
+    CONSTRAINT [PK_DIM_INVENTDIM] PRIMARY KEY NONCLUSTERED (
+        [DW_Id] ASC
+    ) NOT ENFORCED
+)
+WITH (HEAP, DISTRIBUTION = ROUND_ROBIN);
