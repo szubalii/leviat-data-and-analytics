09e31ff35b69224ff522d7baa92846f875f48bca
Michiel Pors <you@example.com>
2022-07-12 09:39:42 +0200

    create or alter db scoped cred

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/Security/Database Scoped Credentials/dsc_SAS_OrchDB.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/Security/Database Scoped Credentials/dsc_SAS_OrchDB.sql
@@ -1,3 +1,18 @@
-CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
-WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = '$(sasToken_OrchDB)';
+-- First check if the database scoped credential already exists.
+-- If not, create it, if so, alter it.
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.database_scoped_credentials
+    WHERE name = 'dsc_SAS_OrchDB'
+)
+BEGIN
+    CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+    WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+    SECRET = '$(sasToken_OrchDB)';
+END
+ELSE
+BEGIN
+    ALTER DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+    WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+    SECRET = '$(sasToken_OrchDB)';
+END
