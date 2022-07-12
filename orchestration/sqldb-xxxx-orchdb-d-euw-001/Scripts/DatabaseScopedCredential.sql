4f0c27537a1d00d1f893d37e2ace027bac858b06
Michiel Pors <you@example.com>
2022-07-12 11:08:06 +0200

    update post deploy

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/Scripts/DatabaseScopedCredential.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/Scripts/DatabaseScopedCredential.sql
@@ -1,16 +1,61 @@
-﻿CREATE MASTER KEY ENCRYPTION
-BY PASSWORD = '$(masterKeyPwd)';
+﻿IF EXISTS (
+  SELECT 1
+  FROM sys.database_scoped_credentials
+  WHERE name = 'dsc_SAS_OrchDB'
+)
+BEGIN
+  ALTER DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+  WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+  SECRET = '$(sasToken_OrchDB)';
+END
+ELSE
+BEGIN
+  CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+  WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+  SECRET = '$(sasToken_OrchDB)';
+END
 GO
 
-CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
-WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = '$(sasToken_OrchDB)';
+IF EXISTS (
+  SELECT 1
+  FROM sys.external_data_sources
+  WHERE name = 'eds_OrchDB'
+)
+BEGIN
+  ALTER EXTERNAL DATA SOURCE eds_OrchDB SET
+    LOCATION = '$(containerURL)', --https://$(storageAccount).blob.core.windows.net/orch-db',
+    CREDENTIAL= dsc_SAS_OrchDB;
+END
+ELSE
+BEGIN
+  CREATE EXTERNAL DATA SOURCE eds_OrchDB
+  WITH (
+    TYPE = BLOB_STORAGE,
+    LOCATION = '$(containerURL)', --https://$(storageAccount).blob.core.windows.net/orch-db',
+    CREDENTIAL= dsc_SAS_OrchDB
+  );
+END
 GO
 
-CREATE EXTERNAL DATA SOURCE eds_OrchDB
-WITH (
-  TYPE = BLOB_STORAGE,
-  LOCATION = '$(containerURL)',--https://$(storageAccount).blob.core.windows.net/orch-db',
-  CREDENTIAL= dsc_SAS_OrchDB
-);
-GO
+-- IF EXISTS (
+
+-- )
+--   DROP MASTER KEY;
+-- GO
+
+-- CREATE MASTER KEY ENCRYPTION
+-- BY PASSWORD = '$(masterKeyPwd)';
+-- GO
+
+-- CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+-- WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+-- SECRET = '$(sasToken_OrchDB)';
+-- GO
+
+-- CREATE EXTERNAL DATA SOURCE eds_OrchDB
+-- WITH (
+--   TYPE = BLOB_STORAGE,
+--   LOCATION = '$(containerURL)', --https://$(storageAccount).blob.core.windows.net/orch-db',
+--   CREDENTIAL= dsc_SAS_OrchDB
+-- );
+-- GO
