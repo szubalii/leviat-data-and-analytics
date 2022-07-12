560a07bf432d33d673b123475763ef7f44720884
Michiel Pors <you@example.com>
2022-07-12 10:20:50 +0200

    revert dsc_SAS_OrchDB

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/Security/Database Scoped Credentials/dsc_SAS_OrchDB.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/Security/Database Scoped Credentials/dsc_SAS_OrchDB.sql
@@ -1,18 +1,3 @@
--- First check if the database scoped credential already exists.
--- If not, create it, if so, alter it.
-IF NOT EXISTS (
-    SELECT *
-    FROM sys.database_scoped_credentials
-    WHERE name = 'dsc_SAS_OrchDB'
-)
-BEGIN
-    CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
-    WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-    SECRET = '$(sasToken_OrchDB)'
-END
-ELSE
-BEGIN
-    ALTER DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
-    WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-    SECRET = '$(sasToken_OrchDB)'
-END;
+CREATE DATABASE SCOPED CREDENTIAL dsc_SAS_OrchDB
+WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
+SECRET = '$(sasToken_OrchDB)';
