5e70b1f5a500ce87187a03594237a0d3105b469b
Barannik Raisa <barannik.rb@pg.com>
2022-07-28 15:42:59 +0300

    switch MaterialGroup to ProductGroup

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_Product.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_Product.sql
@@ -10,7 +10,7 @@ SELECT
 	, [MaterialTypeID]
 	, [MaterialType]
 	, [CrossPlantStatus]
-	, [MaterialGroup]
+	, [ProductGroup]
 	, [ProductHierarchy]
 	, [Product_L1_PillarID]
 	, [Product_L2_GroupID]
