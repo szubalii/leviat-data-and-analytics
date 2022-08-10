fcf60e9afb41e2b30f392a428223b593a4a95326
Tamara Dorodnova <tdorodnova@crh.com>
2022-08-10 11:33:43 +0400

    change primary key

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/map_AXBI/Tables/StockUnit.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_AXBI/Tables/StockUnit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [map_AXBI].[StockUnit]
 ( 
     [AXUnit]            NVARCHAR(3)   NOT NULL,
-    [AXUnitname]        NVARCHAR(30)    NOT NULL,
+    [AXUnitname]        NVARCHAR(30)  NOT NULL,
     [SAPUnit]           NVARCHAR(3)   NOT NULL,
     [SAPUoM]            NVARCHAR(4)   NOT NULL,
     [t_applicationId]   VARCHAR(32)    NULL,
@@ -9,7 +9,7 @@ CREATE TABLE [map_AXBI].[StockUnit]
     [t_jobDtm]          DATETIME       NULL,
     [t_jobBy]           NVARCHAR(128)  NULL,
     [t_filePath]        NVARCHAR(1024) NULL,
-    CONSTRAINT [PK_StoclUnit] PRIMARY KEY NONCLUSTERED ([AXUnit],[SAPUnit]) NOT ENFORCED
+    CONSTRAINT [PK_StoclUnit] PRIMARY KEY NONCLUSTERED ([AXUnit],[AXUnitname],[SAPUnit],[SAPUoM]) NOT ENFORCED
 )
 WITH
 (
