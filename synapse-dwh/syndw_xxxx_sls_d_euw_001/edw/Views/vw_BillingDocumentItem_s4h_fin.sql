77ff9a83ead9346f7ed18fd1c6c29181675eb20c
Tamara Dorodnova <tdorodnova@crh.com>
2022-08-24 19:05:03 +0400

    fix MaterialID

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_BillingDocumentItem_s4h_fin.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_BillingDocumentItem_s4h_fin.sql
@@ -1135,7 +1135,7 @@ BDwithConditionAmountFreight AS (
     ,   NULL AS [FinNetAmountRealProduct]
     ,   CASE
             WHEN 
-                [Material] = '70000011'
+                [Material] = '000000000070000011'
                 AND
                 [FinNetAmountSumBD] != 0
                 AND
@@ -1147,9 +1147,9 @@ BDwithConditionAmountFreight AS (
     ,   NULL AS [FinNetAmountMinQty]
     ,   CASE
             WHEN 
-                ([Material] = '70000010'
+                ([Material] = '000000000070000010'
                 OR
-                [Material] = '70000051')
+                [Material] = '000000000070000051')
                 AND
                 [FinNetAmountSumBD] != 0
                 AND
@@ -1162,7 +1162,7 @@ BDwithConditionAmountFreight AS (
 --  ,   BDwithZVER.NetAmountZVER -- MPS 2021/11/04: removed as NetAmountZVER same as NetAmountVerp
     ,   CASE
             WHEN 
-                [Material] NOT IN ('70000010','70000051','70000011')
+                [Material] NOT IN ('000000000070000010','000000000070000051','000000000070000011')
                 AND
                 [FinNetAmountSumBD] != 0
                 AND
