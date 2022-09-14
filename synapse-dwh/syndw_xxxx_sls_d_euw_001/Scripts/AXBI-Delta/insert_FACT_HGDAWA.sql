b060c1efb896156fa54fe5ab1a20326fed3d3b31
Michiel Pors <you@example.com>
2022-09-14 10:42:40 +0200

    sql script for bulk insert FACT_HGDAWA

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/Scripts/AXBI-Delta/insert_FACT_HGDAWA.sql
@@ -0,0 +1,19 @@
+﻿BULK INSERT [base_dw_halfen_2_dwh].[FACT_HGDAWA_active]
+FROM 'C:\Users\Michiel_Pors\Downloads\FACT_HGDAWA_2022_01_27_09_43_53_650.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	FIELDQUOTE = '"'
+)
+
+BULK INSERT [base_dw_halfen_2_dwh].[FACT_HGDAWA_new]
+FROM 'C:\Users\Michiel_Pors\Downloads\FACT_HGDAWA_2022_01_31_09_35_46_452.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	FIELDQUOTE = '"'
+)
+
+truncate table [base_dw_halfen_2_dwh].[FACT_HGDAWA_active]
+truncate table [base_dw_halfen_2_dwh].[FACT_HGDAWA_new]
