06d4512a599d1b112ae37f7f173868dc92f9b05b
Michiel Pors <you@example.com>
2022-09-15 16:30:28 +0200

    correct order

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_Bank.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_Bank.sql
@@ -20,12 +20,12 @@ CREATE TABLE [base_s4h_cax].[I_Bank]
 , [CheckDigitCalculationMethod] nvarchar(4) collate Latin1_General_100_BIN2
 , [BankDataFileFormat] nvarchar(3) collate Latin1_General_100_BIN2
 , [AddressID] nvarchar(10) collate Latin1_General_100_BIN2
-, [t_applicationId]       VARCHAR (32)
-, [t_jobId]               VARCHAR (36)
-, [t_jobDtm]             DATETIME
-, [t_jobBy]        VARCHAR (20)
-, [t_filePath]            NVARCHAR (1024)
-, [t_extractionDtm]             DATETIME
+, [t_applicationId] VARCHAR (32)
+, [t_jobId]         VARCHAR (36)
+, [t_jobDtm]        DATETIME
+, [t_jobBy]         NVARCHAR (128)
+, [t_extractionDtm] DATETIME
+, [t_filePath]      NVARCHAR (1024)
 , CONSTRAINT [PK_I_Bank] PRIMARY KEY NONCLUSTERED([MANDT],[BankCountry],[BankInternalID]) NOT ENFORCED 
 )
 WITH ( 
