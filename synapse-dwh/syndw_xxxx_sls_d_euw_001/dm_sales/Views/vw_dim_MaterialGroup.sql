01a177873c093246586388a9e02154a57a6b7be9
Fred Bouw <fbouw@crh.com>
2022-09-29 10:25:01 +0200

    Created new DM view for the Procurement data set

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_MaterialGroup.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dm_sales].[vw_dim_MaterialGroup] AS 
+
+SELECT
+  [MaterialGroupID]
+, [MaterialGroup]
+, [MaterialAuthorizationGroup]
+, [MaterialGroupText]
+, [t_applicationId]
+
+FROM [edw].[dim_MaterialGroup]
