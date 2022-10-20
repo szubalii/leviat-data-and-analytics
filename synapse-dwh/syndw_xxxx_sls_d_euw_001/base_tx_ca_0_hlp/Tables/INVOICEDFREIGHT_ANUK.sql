7dc402bb5ed983f7fde46a9fd924fc5b080a5c90
Anastasiya Buniashyna <Anastasiya_Buniashyna@epam.com>
2022-10-20 11:49:31 +0300

    add tech fields, pk, change case, append entity file

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_tx_ca_0_hlp/Tables/INVOICEDFREIGHT_ANUK.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_tx_ca_0_hlp/Tables/INVOICEDFREIGHT_ANUK.sql
@@ -1,6 +1,14 @@
 ﻿CREATE TABLE [base_tx_ca_0_hlp].[INVOICEDFREIGHT_ANUK](
-	[Accountingdate] [datetime] NULL,
-	[InvoicedFreightLocal] [decimal](38, 12) NULL,
-	[PackingSlipID] [nvarchar](20) NULL
+	[Accountingdate] [DATETIME] NOT NULL,
+	[InvoicedFreightLocal] [DECIMAL](38, 12) NOT NULL,
+	[PackingSlipID] [NVARCHAR](20) NOT NULL,
+	[t_applicationId] [VARCHAR](32) NULL,
+    [t_jobId] [VARCHAR](36) NULL,
+    [t_jobDtm] [DATETIME],
+    [t_jobBy] [NVARCHAR](128) NULL,
+    [t_extractionDtm] [DATETIME],
+    [t_filePath] [NVARCHAR](1024) NULL,
+    CONSTRAINT [PK_INVOICEDFREIGHT_ANUK] PRIMARY KEY NONCLUSTERED (
+    [Accountingdate], [InvoicedFreightLocal], [PackingSlipID]
 )  NOT ENFORCED
 ) WITH (HEAP, DISTRIBUTION = ROUND_ROBIN)
