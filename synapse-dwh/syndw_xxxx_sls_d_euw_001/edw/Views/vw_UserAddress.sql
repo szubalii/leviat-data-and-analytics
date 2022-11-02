0d89c0a8eae77decac51f7d328acba6ed1fe40f6
Fred Bouw <fbouw@crh.com>
2022-11-02 11:35:24 +0100

    Corrected for missing column in EDW table

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_UserAddress.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_UserAddress.sql
@@ -5,9 +5,6 @@ AS
 	, [PERSNUMBER] as [UserID]
 	, [CLASS] as [UserClass]
 	, [t_applicationId]
-	, [t_jobId]
-	, [t_jobDtm]
-	, [t_jobBy]
 	, [t_extractionDtm]
 
 	FROM [base_s4h_cax].[P_UserAddress]
