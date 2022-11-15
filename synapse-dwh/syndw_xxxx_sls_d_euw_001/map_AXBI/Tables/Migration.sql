ef39823fddd19a94a69e2a7519255f3bd9a8eb1c
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2022-11-15 18:20:37 +0400

    Added t_applicationId to Migration table

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/map_AXBI/Tables/Migration.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_AXBI/Tables/Migration.sql
@@ -2,6 +2,7 @@ CREATE TABLE [map_AXBI].[Migration]
 (
     [DataAreaID]                nvarchar(4),
     [MigrationDate]             date,
+    [t_applicationId]           VARCHAR(32)    NULL,
     [t_jobId]                   VARCHAR(36)    NULL,
     [t_jobDtm]                  DATETIME       NULL,
     [t_jobBy]                   NVARCHAR(128)  NULL,
