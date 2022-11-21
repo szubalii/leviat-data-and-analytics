e8516efebb6f2c76a4f53178896580de865f4174
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2022-11-21 13:49:47 +0400

    Changed USD rate calculation

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ACDOCA_SalesView.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ACDOCA_SalesView.sql
@@ -1,24 +1,28 @@
 CREATE VIEW [edw].[vw_ACDOCA_SalesView]
 AS
-WITH ExchangeRate AS (
+WITH AllRates AS (
     SELECT 
         [SourceCurrency]
         ,[TargetCurrency]
         ,[ExchangeRateEffectiveDate]
         , COALESCE(DATEADD(day, -1,LEAD([ExchangeRateEffectiveDate]) OVER (PARTITION BY [SourceCurrency],[TargetCurrency] ORDER BY [ExchangeRateEffectiveDate])),'9999-12-31') AS LastDay
         ,[ExchangeRate]
-        , CASE
-            WHEN TargetCurrency = 'EUR'
-                THEN 30
-            WHEN TargetCurrency = 'USD'
-                THEN 40
-        END AS CurrencyTypeID
     FROM 
         edw.dim_ExchangeRates
     WHERE
         [ExchangeRateType] = 'ZAXBIBUD'
         and
-        [TargetCurrency] IN ('EUR','USD')
+        [TargetCurrency] = 'EUR')
+,ExchangeRate AS (
+    SELECT 
+        [SourceCurrency]
+        ,[TargetCurrency]
+        ,[ExchangeRateEffectiveDate]
+        ,[LastDay]
+        ,[ExchangeRate]
+        , 30 AS CurrencyTypeID
+    FROM 
+        AllRates
 UNION ALL
     SELECT [SourceCurrency]
         , [SourceCurrency]
@@ -31,19 +35,26 @@ UNION ALL
     GROUP BY
         [SourceCurrency]
 UNION ALL
-    SELECT 'USD'
+    SELECT other_currency.SourceCurrency
         , 'USD'
-        , CAST('1900-01-01' AS date)
-        , CAST('9999-12-31' AS date)
-        , 1.0
+        , CASE
+            WHEN rate2usd.ExchangeRateEffectiveDate > other_currency.ExchangeRateEffectiveDate
+                THEN rate2usd.ExchangeRateEffectiveDate
+            ELSE other_currency.ExchangeRateEffectiveDate
+        END         -- substitute for GREATEST function
+        , CASE
+            WHEN rate2usd.LastDay < other_currency.LastDay
+                THEN rate2usd.LastDay
+            ELSE
+                other_currency.LastDay
+        END         -- substitute for LEAST function
+        , other_currency.ExchangeRate/rate2usd.ExchangeRate
         , 40
-/*UNION ALL
-    SELECT 'EUR'
-        , 'EUR'
-        , CAST('1900-01-01' AS date)
-        , CAST('9999-12-31' AS date)
-        , 1.0
-        , 30  */  
+    FROM AllRates other_currency
+    INNER JOIN AllRates rate2usd
+        ON rate2usd.ExchangeRateEffectiveDate BETWEEN other_currency.ExchangeRateEffectiveDate AND other_currency.LastDay
+        AND rate2usd.LastDay <= other_currency.LastDay
+    WHERE rate2usd.SourceCurrency = 'USD'
 )
 SELECT 
     GLALIRD.[SourceLedger]                           AS [SourceLedgerID],
