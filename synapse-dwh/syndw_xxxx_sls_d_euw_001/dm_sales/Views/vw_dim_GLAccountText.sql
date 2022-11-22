39a4884f591f0d5028c542cfa4678429ad954692
Fred Bouw <fbouw@crh.com>
2022-11-22 10:15:55 +0100

    Update for issue with incorrect schema

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_GLAccountText.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_GLAccountText.sql
@@ -1,1 +1,11 @@
-﻿
+﻿CREATE VIEW [dm_sales].[vw_dim_GLAccountText]
+    AS SELECT
+        ChartOfAccounts,
+        GLAccount,
+        GLAccountName,
+        GLAccountLongName,
+        LastChangeDateTime,
+        t_applicationId,
+        t_extractionDtm
+    FROM [base_s4h_cax].[I_GLAccountText]
+    WHERE [Language] = 'E'        
