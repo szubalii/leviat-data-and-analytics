b38765418b4f7681ed3ddb95de095381ebf88b6b
Michiel Pors <you@example.com>
2022-11-28 14:22:16 +0100

    update brand

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_Brand.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_Brand.sql
@@ -1,7 +1,7 @@
 CREATE VIEW [edw].[vw_Brand] AS
 SELECT
-    AMGT.[AdditionalMaterialGroup1]         as [BrandID]
-,   AMGT.[AdditionalMaterialGroup1Name]     as [Brand]
+    AMGT.[AdditionalMaterialGroup1]     as [BrandID]
+,   AMGT.[AdditionalMaterialGroup1Name] as [Brand]
 ,   AMGT.t_applicationId
 FROM
     [base_s4h_cax].[I_AdditionalMaterialGroup1Text] AMGT
