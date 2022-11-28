810849f8e89b222f1b3743cdcb878af58ee4f9e1
Michiel Pors <you@example.com>
2022-11-28 16:39:25 +0100

    store test in artifact

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_Brand.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_Brand.sql
@@ -1,8 +1,8 @@
 CREATE VIEW [edw].[vw_Brand] AS
 SELECT
     AMGT.[AdditionalMaterialGroup1]     AS [BrandID],
-    AMGT.[AdditionalMaterialGroup1Name] AS [Brand]
-,   AMGT.t_applicationId
+    AMGT.[AdditionalMaterialGroup1Name] AS [Brand],
+    AMGT.t_applicationId
 FROM
     [base_s4h_cax].[I_AdditionalMaterialGroup1Text] AMGT
 WHERE
