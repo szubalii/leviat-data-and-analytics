a87740de4a570da5c83856f2ac1474db69fdc43d
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2022-11-30 11:15:35 +0300

    Changed source views and tables Added description to dummies Minor fixes

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_dim_Customer.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_dim_Customer.sql
@@ -1,5 +1,16 @@
 CREATE VIEW [dm_finance].[vw_dim_Customer] AS
-
+WITH DummyRecords AS (
+    SELECT CONCAT('(MA)-',[LowerBoundaryAccount])   AS DummyID
+        ,[LowerBoundaryAccount]                     AS GLAccountID
+    FROM edw.vw_FinancialStatementHierarchy
+    WHERE FinancialStatementItem IN (
+        SELECT FinancialStatementItem
+        FROM edw.vw_FinancialStatementItem
+        WHERE 
+            ParentNode = '001005'               -- for DEV
+            --ParentNode = '000570'             -- for PROD
+    )
+)
 SELECT
   [CustomerID]
 , [Customer]
@@ -33,33 +44,36 @@ FROM [edw].[dim_Customer]
 UNION ALL
 
 SELECT
-  'MA-Dummy'    AS [CustomerID]
-, [ProductID]
-, [ProductID]   AS [CustomerID_Name]
-, null          AS [CustomerAccountGroupID]
-, null          AS [AccountGroup]
-, null          AS [AuthorizationGroupID]
-, null          AS AuthorizationGroup
-, null          AS [DeliveryIsBlocked]
-, null          AS [PostingIsBlocked]
-, null          AS [BillingIsBlockedForCustomer]
-, null          AS [OrderIsBlockedForCustomer]
-, null          AS [IsOneTimeAccount]
-, null          AS [CountryID]
-, null          AS [Country]
-, [ProductID]
-, null          AS [PostalCode]
-, null          AS [StreetName]
-, null          AS [RegionID]
-, null          AS [Region]
-, null          AS [DeletionIndicator]
-, null          AS [TradingPartnerID]
-, null          AS [TradingPartner]
-, null          AS [BusinessType]
-, null          AS [IndustryType]
-, null          AS [t_jobDtm]
-, null          AS [t_applicationId]
-, null          AS[t_extractionDtm]
-FROM [dm_finance].[vw_fact_ACDOCA_EPMSalesView]
-WHERE BillingDocumentTypeID = 'MA-Dummy'
-GROUP BY [ProductID]
+  'MA-Dummy'        AS [CustomerID]
+,d.[DummyID] COLLATE DATABASE_DEFAULT       AS [Customer]
+,CONCAT(d.[DummyID] COLLATE DATABASE_DEFAULT
+    ,'_'
+    ,gla.[GLAccountLongName]
+)                   AS [CustomerID_Name]
+,null               AS [CustomerAccountGroupID]
+,null               AS [AccountGroup]
+,null               AS [AuthorizationGroupID]
+,null               AS AuthorizationGroup
+,null               AS [DeliveryIsBlocked]
+,null               AS [PostingIsBlocked]
+,null               AS [BillingIsBlockedForCustomer]
+,null               AS [OrderIsBlockedForCustomer]
+,null               AS [IsOneTimeAccount]
+,null               AS [CountryID]
+,null               AS [Country]
+,gla.[GLAccountLongName]    AS CityName
+,null               AS [PostalCode]
+,null               AS [StreetName]
+,null               AS [RegionID]
+,null               AS [Region]
+,null               AS [DeletionIndicator]
+,null               AS [TradingPartnerID]
+,null               AS [TradingPartner]
+,null               AS [BusinessType]
+,null               AS [IndustryType]
+,null               AS [t_jobDtm]
+,null               AS [t_applicationId]
+,null               AS[t_extractionDtm]
+FROM DummyRecords d
+LEFT JOIN [dm_sales].[vw_dim_GLAccountText] gla
+    ON d.GLAccountID = gla.GLAccount                COLLATE DATABASE_DEFAULT
--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_dim_Product.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_dim_Product.sql
@@ -1,5 +1,16 @@
 CREATE VIEW [dm_finance].[vw_dim_Product] AS
-
+WITH DummyRecords AS (
+    SELECT CONCAT('(MA)-',[LowerBoundaryAccount])   AS DummyID
+        ,[LowerBoundaryAccount]                     AS GLAccountID
+    FROM edw.vw_FinancialStatementHierarchy
+    WHERE FinancialStatementItem IN (
+        SELECT FinancialStatementItem
+        FROM edw.vw_FinancialStatementItem
+        WHERE 
+            ParentNode = '001005'               -- for DEV
+            --ParentNode = '000570'             -- for PROD
+    )
+)
 SELECT
   [sk_dim_Product],
   [ProductID],
@@ -141,70 +152,73 @@ FROM
 UNION ALL
 
 SELECT
-  [ProductID] AS [sk_dim_Product],
-  [ProductID],
-  [ProductID],
-  [ProductID],
-  [ProductID],
-  NULL AS [CreationDate],
-  NULL AS [MaterialTypeID],
-  NULL AS [MaterialType],
-  NULL AS [IsMarkedForDeletion],
-  NULL AS [CrossPlantStatus],
-  NULL AS [GrossWeight],
-  NULL AS [SourceOfSupply],
-  NULL AS [CountryOfOrigin],
-  NULL AS [CompetitorID],
-  NULL AS [MaterialGroup],
-  NULL AS [BaseUnit],
-  NULL AS [ItemCategoryGroup],
-  NULL AS [ProductHierarchy],
-  [ProductID]   AS [Product_L1_PillarID],
-  NULL AS [Product_L2_GroupID],
-  NULL AS [Product_L3_TypeID],
-  NULL AS [Product_L4_FamilyID],
-  NULL AS [Product_L5_SubFamilyID],
-  [ProductID]   AS [Product_L1_Pillar],
-  NULL AS [Product_L2_Group],
-  NULL AS [Product_L3_Type],
-  NULL AS [Product_L4_Family],
-  NULL AS [Product_L5_SubFamily],
-  NULL AS [ProductCategory],
-  NULL AS [Brand],
-  NULL AS [IndustryStandardName],
-  NULL AS [ProductStandardID],
-  NULL AS [InternationalArticleNumberCat],
-  NULL AS [ProductIsConfigurable],
-  NULL AS [IsBatchManagementRequired],
-  NULL AS [ExternalProductGroup],
-  NULL AS [CrossPlantConfigurableProduct],
-  NULL AS [ProductManufacturerNumber],
-  NULL AS [ManufacturerNumber],
-  NULL AS [MaximumPackagingLength],
-  NULL AS [MaximumPackagingWidth],
-  NULL AS [MaximumPackagingHeight],
-  NULL AS [MaximumCapacity],
-  NULL AS [OvercapacityTolerance],
-  NULL AS [UnitForMaxPackagingDimensions],
-  NULL AS [BaseUnitSpecificProductLength],
-  NULL AS [BaseUnitSpecificProductWidth],
-  NULL AS [BaseUnitSpecificProductHeight],
-  NULL AS [ArticleCategory],
-  NULL AS [ProductIsLocked],
-  NULL AS [BasicMaterial],
-  NULL AS [SalesProduct],
-  NULL AS [NetWeight],
-  NULL AS [IndustrySector],
-  NULL AS [WeightUnit],
-  NULL AS [Division],
-  NULL AS [TransportationGroup],
-  NULL AS [ZZ1_CustomFieldRiskMit_PRD],
-  NULL AS [ZZ1_CustomFieldHighRis_PRD],
-  NULL AS [ZZ1_CustomFieldRiskRea_PRD],
-  NULL AS [CreatedByUser],
-  NULL AS [t_jobDtm],
-  NULL AS [t_applicationId],
-  NULL AS [t_extractionDtm]
-FROM [dm_finance].[vw_fact_ACDOCA_EPMSalesView]
-WHERE BillingDocumentTypeID = 'MA-Dummy'
-GROUP BY [ProductID]
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [sk_dim_Product],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [ProductID],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [ProductExternalID],
+  gla.[GLAccountLongName]   AS [Product],
+  CONCAT(d.[DummyID]        COLLATE DATABASE_DEFAULT
+    ,'_'
+    ,gla.[GLAccountLongName]
+  )                         AS [ProductID_Name],
+  NULL                      AS [CreationDate],
+  NULL                      AS [MaterialTypeID],
+  NULL                      AS [MaterialType],
+  NULL                      AS [IsMarkedForDeletion],
+  NULL                      AS [CrossPlantStatus],
+  NULL                      AS [GrossWeight],
+  NULL                      AS [SourceOfSupply],
+  NULL                      AS [CountryOfOrigin],
+  NULL                      AS [CompetitorID],
+  NULL                      AS [MaterialGroup],
+  NULL                      AS [BaseUnit],
+  NULL                      AS [ItemCategoryGroup],
+  NULL                      AS [ProductHierarchy],
+  'MA'                      AS [Product_L1_PillarID],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [Product_L2_GroupID],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [Product_L3_TypeID],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [Product_L4_FamilyID],
+  d.[DummyID] COLLATE DATABASE_DEFAULT              AS [Product_L5_SubFamilyID],
+  'Manual Adjustment'       AS [Product_L1_Pillar],
+  gla.[GLAccountLongName]   AS [Product_L2_Group],    
+  gla.[GLAccountLongName]   AS [Product_L3_Type],     
+  gla.[GLAccountLongName]   AS [Product_L4_Family],   
+  gla.[GLAccountLongName]   AS [Product_L5_SubFamily],
+  NULL                      AS [ProductCategory],
+  NULL                      AS [Brand],
+  NULL                      AS [IndustryStandardName],
+  NULL                      AS [ProductStandardID],
+  NULL                      AS [InternationalArticleNumberCat],
+  NULL                      AS [ProductIsConfigurable],
+  NULL                      AS [IsBatchManagementRequired],
+  NULL                      AS [ExternalProductGroup],
+  NULL                      AS [CrossPlantConfigurableProduct],
+  NULL                      AS [ProductManufacturerNumber],
+  NULL                      AS [ManufacturerNumber],
+  NULL                      AS [MaximumPackagingLength],
+  NULL                      AS [MaximumPackagingWidth],
+  NULL                      AS [MaximumPackagingHeight],
+  NULL                      AS [MaximumCapacity],
+  NULL                      AS [OvercapacityTolerance],
+  NULL                      AS [UnitForMaxPackagingDimensions],
+  NULL                      AS [BaseUnitSpecificProductLength],
+  NULL                      AS [BaseUnitSpecificProductWidth],
+  NULL                      AS [BaseUnitSpecificProductHeight],
+  NULL                      AS [ArticleCategory],
+  NULL                      AS [ProductIsLocked],
+  NULL                      AS [BasicMaterial],
+  NULL                      AS [SalesProduct],
+  NULL                      AS [NetWeight],
+  NULL                      AS [IndustrySector],
+  NULL                      AS [WeightUnit],
+  NULL                      AS [Division],
+  NULL                      AS [TransportationGroup],
+  NULL                      AS [ZZ1_CustomFieldRiskMit_PRD],
+  NULL                      AS [ZZ1_CustomFieldHighRis_PRD],
+  NULL                      AS [ZZ1_CustomFieldRiskRea_PRD],
+  NULL                      AS [CreatedByUser],
+  NULL                      AS [t_jobDtm],
+  NULL                      AS [t_applicationId],
+  NULL                      AS [t_extractionDtm]
+FROM DummyRecords d
+LEFT JOIN [dm_sales].[vw_dim_GLAccountText] gla
+    ON d.GLAccountID = gla.GLAccount                COLLATE DATABASE_DEFAULT
