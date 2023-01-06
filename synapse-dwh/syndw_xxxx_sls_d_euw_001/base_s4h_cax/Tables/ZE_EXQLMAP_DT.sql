9f986507ec6d1f620c1f767619564c26acd96c85
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-01-06 19:52:43 +0400

    Added ZE_EXQLMAP_DT table Modified filtration for EPMSalesView

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/ZE_EXQLMAP_DT.sql
@@ -0,0 +1,34 @@
+CREATE TABLE [base_s4h_cax].[ZE_EXQLMAP_DT](
+    [GLACCOUNT]             VARCHAR(12) NOT NULL
+    ,[FUNCTIONALAREA]       VARCHAR(64)
+    ,[TRANSACTIONTYPE]      VARCHAR(64)
+    ,[TRADINGPARTNER]       VARCHAR(64)
+    ,[CONTIGENCY1]          VARCHAR(64)
+    ,[CONTIGENCY2]          VARCHAR(64)
+    ,[CONTIGENCY3]          VARCHAR(64)
+    ,[CONTIGENCY4]          VARCHAR(64)
+    ,[CONTIGENCY5]          VARCHAR(64)
+    ,[REKNR]                VARCHAR(64)
+    ,[REKOMS]               VARCHAR(64)
+    ,[DI1NR]                VARCHAR(64)
+    ,[DI2NR]                VARCHAR(64)
+    ,[REFNR]                VARCHAR(64)
+    ,[ORGNR_COR]            VARCHAR(64)
+    ,[TRANR]                VARCHAR(64)
+    ,[CATNR]                VARCHAR(64)
+    ,[JNLOMS]               VARCHAR(64)
+    ,[XLSBLD]               VARCHAR(64)
+    ,[t_applicationId]       VARCHAR(32)    NULL
+    ,[t_jobId]               VARCHAR(36)    NULL
+    ,[t_jobDtm]              DATETIME       NULL
+    ,[t_jobBy]               NVARCHAR(128)  NULL
+    ,[t_filePath]            NVARCHAR(1024) NULL
+    , CONSTRAINT [PK_ZE_EXQLMAP_DT] PRIMARY KEY NONCLUSTERED (
+    [GLACCOUNT],[FUNCTIONALAREA]
+  ) NOT ENFORCED
+)
+WITH (
+  HEAP
+)
+
+
