307c25e034a0d0f9d0d1f335d71151dba61d879e
Michiel Pors <you@example.com>
2023-01-17 13:19:34 +0100

    add [utilities].[t_field_values]

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/utilities/Tables/t_field_values.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [utilities].[t_field_values] (
+    [table_id]      INT             NOT NULL,
+    [default_field]  VARCHAR  (30)  NOT NULL,
+    [index]         TINYINT         NOT NULL,
+    [default_value] NVARCHAR(1024)  NOT NULL
+)
+WITH (HEAP, DISTRIBUTION = REPLICATE);
