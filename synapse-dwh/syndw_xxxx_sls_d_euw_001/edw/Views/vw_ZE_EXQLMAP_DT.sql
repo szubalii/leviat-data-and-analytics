d684cfc5f3dfe1a7f3a5e8e3d35198f46706cfa6
Nikolai Kukhtenko <nkukhtenko@crh.com>
2023-03-02 13:44:37 +0000

    Revert "Merged PR 2025: New fields, changed TP logic, new mapping"

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ZE_EXQLMAP_DT.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ZE_EXQLMAP_DT.sql
@@ -5,7 +5,6 @@ SELECT
     ,COALESCE(FUNCTIONALAREA,'')                AS FUNCTIONALAREA   --Data in [base_s4h_cax].[ZE_EXQLMAP_DT] is 
 --currently ingested from csv-file using COPY INTO in which empty strings are converted to NULL values by default.
 -- [edw].[fact_ACDOCA] is extracted using Theobald that doesn't generate NULL values in parquet files for empty strings.
-    ,MAX(CONTIGENCY4) AS CONTIGENCY4
     ,MAX(CONTIGENCY5) AS CONTIGENCY5
 FROM [base_s4h_cax].[ZE_EXQLMAP_DT]
 WHERE [CONTIGENCY5] <> ''
