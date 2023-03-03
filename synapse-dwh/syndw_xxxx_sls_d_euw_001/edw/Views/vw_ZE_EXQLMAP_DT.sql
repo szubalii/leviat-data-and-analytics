a91ce81665ab23f94ab3ee02901ab9a0aefc757a
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-03-03 16:57:47 +0400

    Fake changes for vcs

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ZE_EXQLMAP_DT.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ZE_EXQLMAP_DT.sql
@@ -5,8 +5,8 @@ SELECT
     ,COALESCE(FUNCTIONALAREA,'')                AS FUNCTIONALAREA   --Data in [base_s4h_cax].[ZE_EXQLMAP_DT] is 
 --currently ingested from csv-file using COPY INTO in which empty strings are converted to NULL values by default.
 -- [edw].[fact_ACDOCA] is extracted using Theobald that doesn't generate NULL values in parquet files for empty strings.
-    ,MAX(CONTIGENCY4) AS CONTIGENCY4
-    ,MAX(CONTIGENCY5) AS CONTIGENCY5
+    ,MAX(CONTIGENCY4) AS CONTIGENCY4 
+    ,MAX(CONTIGENCY5) AS CONTIGENCY5 
 FROM [base_s4h_cax].[ZE_EXQLMAP_DT]
 WHERE [CONTIGENCY5] <> ''
     AND XLSBLD = 'IS'
