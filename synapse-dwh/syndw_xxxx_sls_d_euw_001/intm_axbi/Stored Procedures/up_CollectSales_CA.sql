64e8d776b0a8f4e4d89df670add949b434c0eff8
Solomiya Leshchuk <sleshchuk@crh.com>
2023-03-05 21:19:20 +0200

    excluded entities from sp,add new changes

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/intm_axbi/Stored Procedures/up_CollectSales_CA.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/intm_axbi/Stored Procedures/up_CollectSales_CA.sql
@@ -40,7 +40,7 @@ BEGIN
 
 	--EXEC intm_axbi.up_ReadSales_ANDE @P_Year, @P_Month, @t_jobId, @t_jobDtm, @t_jobBy, @axbiDataBaseEnvSuffix
 
-	--EXEC intm_axbi.up_ReadSales_ANAU @P_Year, @P_Month, @t_jobId, @t_jobDtm, @t_jobBy, @axbiDataBaseEnvSuffix
+	EXEC intm_axbi.up_ReadSales_ANAU @P_Year, @P_Month, @t_jobId, @t_jobDtm, @t_jobBy, @axbiDataBaseEnvSuffix
 	EXEC intm_axbi.up_ReadSales_ANAH @P_Year, @P_Month, @t_jobId, @t_jobDtm, @t_jobBy, @axbiDataBaseEnvSuffix
 	EXEC intm_axbi.up_ReadSales_ANNZ @P_Year, @P_Month, @t_jobId, @t_jobDtm, @t_jobBy, @axbiDataBaseEnvSuffix
 	
--- synapse-dwh/syndw_xxxx_sls_d_euw_001/intm_axbi/Stored Procedures/up_ReadSales_HALF.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/intm_axbi/Stored Procedures/up_ReadSales_HALF.sql
@@ -57,44 +57,44 @@ BEGIN
 	,t_jobBy
 	,t_extractionDtm)
 	select case DACONO
-	       when '5300' then 'HADP'
-		   when '5302' then 'HAIN' 
-		   when '5303' then 'HADE' 
+	       --when '5300' then 'HADP'
+		   --when '5302' then 'HAIN' 
+		   --when '5303' then 'HADE' 
 		   when '5307' then 'HAFR' 
-		   when '5308' then 'HASE' 
+		   --when '5308' then 'HASE' 
 		   when '5309' then 'HABE' 
 		   when '5310' then 'HAUK' 
-		   when '5311' then 'HACH' 
-		   when '5313' then 'HAAT' 
-		   when '5314' then 'HANL' 
-		   when '5315' then 'HAIT' 
-		   when '5316' then 'HAPL' 
-		   when '5317' then 'HACZ' 
-		   when '5320' then 'HAES' 
-		   when '5321' then 'HAPP' 
+		   --when '5311' then 'HACH' 
+		   --when '5313' then 'HAAT' 
+		   --when '5314' then 'HANL' 
+		   --when '5315' then 'HAIT' 
+		   --when '5316' then 'HAPL' 
+		   --when '5317' then 'HACZ' 
+		   --when '5320' then 'HAES' 
+		   --when '5321' then 'HAPP' 
 		   when '5325' then 'HANO' 
-		   when '5327' then 'HACN' 
+		   --when '5327' then 'HACN' 
 		   when '5330' then 'HAUS'
 		   else DACONO end,
 	DAORNO, DAIVNO, DAPONR, ISNULL(DASETK, ' '), DAACDT,
 	 case DACONO
-	       when '5300' then 'HADP-' + DACUNO
-		   when '5302' then 'HAIN-' + DACUNO 
-		   when '5303' then 'HADE-' + DACUNO 
+	       --when '5300' then 'HADP-' + DACUNO
+		   --when '5302' then 'HAIN-' + DACUNO 
+		   --when '5303' then 'HADE-' + DACUNO 
 		   when '5307' then 'HAFR-' + DACUNO 
-		   when '5308' then 'HASE-' + DACUNO 
+		   --when '5308' then 'HASE-' + DACUNO 
 		   when '5309' then 'HABE-' + DACUNO 
 		   when '5310' then 'HAUK-' + DACUNO 
-		   when '5311' then 'HACH-' + DACUNO 
-		   when '5313' then 'HAAT-' + DACUNO 
-		   when '5314' then 'HANL-' + DACUNO 
-		   when '5315' then 'HAIT-' + DACUNO 
-		   when '5316' then 'HAPL-' + DACUNO 
-		   when '5317' then 'HACZ-' + DACUNO 
-		   when '5320' then 'HAES-' + DACUNO 
-		   when '5321' then 'HAPP-' + DACUNO 
+		   --when '5311' then 'HACH-' + DACUNO 
+		   --when '5313' then 'HAAT-' + DACUNO 
+		   --when '5314' then 'HANL-' + DACUNO 
+		   --when '5315' then 'HAIT-' + DACUNO 
+		   --when '5316' then 'HAPL-' + DACUNO 
+		   --when '5317' then 'HACZ-' + DACUNO 
+		   --when '5320' then 'HAES-' + DACUNO 
+		   --when '5321' then 'HAPP-' + DACUNO 
 		   when '5325' then 'HANO-' + DACUNO 
-		   when '5327' then 'HACN-' + DACUNO 
+		   --when '5327' then 'HACN-' + DACUNO 
 		   when '5330' then 'HAUS-' + DACUNO
 		   else DACONO end,
 	'HALF-' + DAITNO, substring(DADRID, 1, 2), ISNULL(DADLIX, ' '), ISNULL(DAIVQT,0), DASAAM, DASAAC, DAOSAL, DAOSAC, DAALOL, DAALOC, DA100L, DA100C, DAVSCL + isnull(DAVE10,0), DAVSCC + isnull(DCVE10,0), DAHCOS, DAHCOC,
