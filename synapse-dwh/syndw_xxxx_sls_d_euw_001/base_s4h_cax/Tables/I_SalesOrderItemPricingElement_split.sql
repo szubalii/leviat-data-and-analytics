e4acde3c3988b5875244c3f046f19b91d0b3cae5
Michiel Pors <you@example.com>
2023-03-21 09:06:54 +0100

    all caps fields for I_SalesOrderItemPricingElement_split

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_SalesOrderItemPricingElement_split.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_SalesOrderItemPricingElement_split.sql
@@ -1,47 +1,47 @@
 CREATE TABLE [base_s4h_cax].[I_SalesOrderItemPricingElement_split](
   [MANDT] char(3) collate Latin1_General_100_BIN2 NOT NULL
-, [SalesOrder] nvarchar(10) NOT NULL
-, [SalesOrderItem] char(6) collate Latin1_General_100_BIN2 NOT NULL
-, [PricingProcedureStep] char(3) collate Latin1_General_100_BIN2 NOT NULL
-, [PricingProcedureCounter] char(3) collate Latin1_General_100_BIN2 NOT NULL
-, [ConditionApplication] nvarchar(2)
-, [ConditionType] nvarchar(4)
-, [PricingDateTime] nvarchar(14)
-, [ConditionCalculationType] nvarchar(3)
-, [ConditionBaseValue] decimal(24,9)
-, [ConditionRateValue] decimal(24,9)
-, [ConditionCurrency] char(5) collate Latin1_General_100_BIN2
-, [ConditionQuantity] decimal(5)
-, [ConditionQuantityUnit] nvarchar(3) collate Latin1_General_100_BIN2
-, [ConditionCategory] nvarchar(1)
-, [ConditionIsForStatistics] nvarchar(1)
-, [PricingScaleType] nvarchar(1)
-, [IsRelevantForAccrual] nvarchar(1)
-, [CndnIsRelevantForInvoiceList] nvarchar(1)
-, [ConditionOrigin] nvarchar(1)
-, [IsGroupCondition] nvarchar(1)
-, [ConditionRecord] nvarchar(10)
-, [ConditionSequentialNumber] char(3) collate Latin1_General_100_BIN2
-, [TaxCode] nvarchar(2)
-, [WithholdingTaxCode] nvarchar(2)
-, [CndnRoundingOffDiffAmount] decimal(5,2)
-, [ConditionAmount] decimal(15,2)
-, [TransactionCurrency] char(5) collate Latin1_General_100_BIN2
-, [ConditionControl] nvarchar(1)
-, [ConditionInactiveReason] nvarchar(1)
-, [ConditionClass] nvarchar(1)
-, [PrcgProcedureCounterForHeader] char(3) collate Latin1_General_100_BIN2
-, [FactorForConditionBasisValue] float
-, [StructureCondition] nvarchar(1)
-, [PeriodFactorForCndnBasisValue] float
-, [PricingScaleBasis] nvarchar(3)
-, [ConditionScaleBasisValue] decimal(24,9)
-, [ConditionScaleBasisUnit] nvarchar(3) collate Latin1_General_100_BIN2
-, [ConditionScaleBasisCurrency] char(5) collate Latin1_General_100_BIN2
-, [CndnIsRelevantForIntcoBilling] nvarchar(1)
-, [ConditionIsManuallyChanged] nvarchar(1)
-, [ConditionIsForConfiguration] nvarchar(1)
-, [VariantCondition] nvarchar(26)
+, [SALESORDER] nvarchar(10) NOT NULL
+, [SALESORDERITEM] char(6) collate Latin1_General_100_BIN2 NOT NULL
+, [PRICINGPROCEDURESTEP] char(3) collate Latin1_General_100_BIN2 NOT NULL
+, [PRICINGPROCEDURECOUNTER] char(3) collate Latin1_General_100_BIN2 NOT NULL
+, [CONDITIONAPPLICATION] nvarchar(2)
+, [CONDITIONTYPE] nvarchar(4)
+, [PRICINGDATETIME] nvarchar(14)
+, [CONDITIONCALCULATIONTYPE] nvarchar(3)
+, [CONDITIONBASEVALUE] decimal(24,9)
+, [CONDITIONRATEVALUE] decimal(24,9)
+, [CONDITIONCURRENCY] char(5) collate Latin1_General_100_BIN2
+, [CONDITIONQUANTITY] decimal(5)
+, [CONDITIONQUANTITYUNIT] nvarchar(3) collate Latin1_General_100_BIN2
+, [CONDITIONCATEGORY] nvarchar(1)
+, [CONDITIONISFORSTATISTICS] nvarchar(1)
+, [PRICINGSCALETYPE] nvarchar(1)
+, [ISRELEVANTFORACCRUAL] nvarchar(1)
+, [CNDNISRELEVANTFORINVOICELIST] nvarchar(1)
+, [CONDITIONORIGIN] nvarchar(1)
+, [ISGROUPCONDITION] nvarchar(1)
+, [CONDITIONRECORD] nvarchar(10)
+, [CONDITIONSEQUENTIALNUMBER] char(3) collate Latin1_General_100_BIN2
+, [TAXCODE] nvarchar(2)
+, [WITHHOLDINGTAXCODE] nvarchar(2)
+, [CNDNROUNDINGOFFDIFFAMOUNT] decimal(5,2)
+, [CONDITIONAMOUNT] decimal(15,2)
+, [TRANSACTIONCURRENCY] char(5) collate Latin1_General_100_BIN2
+, [CONDITIONCONTROL] nvarchar(1)
+, [CONDITIONINACTIVEREASON] nvarchar(1)
+, [CONDITIONCLASS] nvarchar(1)
+, [PRCGPROCEDURECOUNTERFORHEADER] char(3) collate Latin1_General_100_BIN2
+, [FACTORFORCONDITIONBASISVALUE] float
+, [STRUCTURECONDITION] nvarchar(1)
+, [PERIODFACTORFORCNDNBASISVALUE] float
+, [PRICINGSCALEBASIS] nvarchar(3)
+, [CONDITIONSCALEBASISVALUE] decimal(24,9)
+, [CONDITIONSCALEBASISUNIT] nvarchar(3) collate Latin1_General_100_BIN2
+, [CONDITIONSCALEBASISCURRENCY] char(5) collate Latin1_General_100_BIN2
+, [CNDNISRELEVANTFORINTCOBILLING] nvarchar(1)
+, [CONDITIONISMANUALLYCHANGED] nvarchar(1)
+, [CONDITIONISFORCONFIGURATION] nvarchar(1)
+, [VARIANTCONDITION] nvarchar(26)
 , [t_applicationId]       VARCHAR (32)
 , [t_jobId]               VARCHAR (36)
 , [t_jobDtm]              DATETIME
