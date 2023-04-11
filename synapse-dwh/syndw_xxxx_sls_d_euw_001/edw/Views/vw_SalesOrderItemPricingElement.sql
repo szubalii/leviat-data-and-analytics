d9c18a5154277a152e16ab99e86c04b955f4784e
Solomiya Leshchuk <sleshchuk@crh.com>
2023-04-11 09:09:06 +0300

    modified:   synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_SalesOrderItemPricingElement.sql

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_SalesOrderItemPricingElement.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_SalesOrderItemPricingElement.sql
@@ -9,7 +9,7 @@ SELECT
 , CONCAT_WS('¦', [SalesOrder] COLLATE SQL_Latin1_General_CP1_CS_AS,[SalesOrderItem] COLLATE SQL_Latin1_General_CP1_CS_AS,'00') as sk_SalesOrderItem
 , CR.[CurrencyTypeID]
 , CR.[CurrencyType]
-, [TransactionCurrency] as [CurrencyID]
+, [TransactionCurrency] COLLATE Latin1_General_100_BIN2 as [CurrencyID]
 , 1.0 as [ExchangeRate]
 , [PricingProcedureStep] 
 , [PricingProcedureCounter] 
@@ -71,7 +71,7 @@ SELECT
 , CONCAT_WS('¦', [SalesOrder] collate SQL_Latin1_General_CP1_CS_AS, [SalesOrderItem] collate SQL_Latin1_General_CP1_CS_AS,'10') as sk_SalesOrderItem
 , CR.[CurrencyTypeID]
 , CR.[CurrencyType]
-, SDI.[CurrencyID]
+, SDI.[CurrencyID] COLLATE Latin1_General_100_BIN2 as CurrencyID
 , (CASE
 			 WHEN SDI.[ExchangeRate] IS NOT NULL
 			 THEN SDI.[ExchangeRate]
