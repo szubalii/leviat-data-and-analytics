01f6fbbbe34c960b5c5782c2d7a959cafd3c3fa4
Solomiya Leshchuk <sleshchuk@crh.com>
2023-04-11 15:33:08 +0300

    added dim table dim_SalesQuotationItemPrcgElmnt

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_SalesQuotationItemPrcgElmnt.sql
@@ -0,0 +1,29 @@
+CREATE TABLE [edw].[dim_SalesQuotationItemPrcgElmnt] 
+(
+    [SalesQuotation] NVARCHAR(10) NOT NULL  
+  , [SalesQuotationItem] CHAR(6) NOT NULL  
+  , [sk_SalesQuotationItem] NVARCHAR(20) NOT NULL 
+  , [ConditionType] NVARCHAR(4) 
+  , [ConditionBaseValue] DECIMAL(24,9)  
+  , [ConditionRateValue] DECIMAL(24,9)  
+  , [ConditionAmount] DECIMAL(15,2)  
+  , [ExchangeRate] DECIMAL(15,6)  
+  , [TransactionCurrencyID] CHAR(5) 
+  , [CurrencyTypeID] CHAR(2)
+  , [CurrencyType] NVARCHAR(20) 
+  , [CurrencyID] CHAR(5)  
+  , [ConditionInactiveReason] NVARCHAR(1)
+  , [t_applicationId] VARCHAR (32)  
+  , [t_jobId] VARCHAR (36)  
+  , [t_jobDtm] DATETIME  
+  , [t_jobBy] VARCHAR (128)  
+  , [t_extractionDtm] DATETIME 
+  , [t_filePath] NVARCHAR (1024)  
+    CONSTRAINT [PK_dim_SalesQuotationItemPrcgElmnt] PRIMARY KEY NONCLUSTERED ([sk_SalesQuotationItem]) NOT ENFORCED
+)
+WITH
+(
+    DISTRIBUTION = REPLICATE,
+    HEAP
+)
+GO
