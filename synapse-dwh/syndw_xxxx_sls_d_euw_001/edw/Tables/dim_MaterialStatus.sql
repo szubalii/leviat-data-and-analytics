2f60027023b91344611b184db6c48a1d53e1296f
Tamara Dorodnova <tdorodnova@crh.com>
2023-04-17 14:23:24 +0400

    add T141

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_MaterialStatus.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [edw].[dim_MaterialStatus]
+(   
+    [MMSTA]                         NCHAR(2)    NOT NULL,
+    [DEINK]                         NCHAR(1),
+    [DSTLK]                         NCHAR(1),
+    [DSTLP]                         NCHAR(1),
+    [DAPLA]                         NCHAR(1),
+    [DPBED]                         NCHAR(1),
+    [DDISP]                         NCHAR(1),
+    [DFAPO]                         NCHAR(1),
+    [DFAKO]                         NCHAR(1),
+    [DINST]                         NCHAR(1),
+    [DBEST]                         NCHAR(1),
+    [DPROG]                         NCHAR(1),
+    [DFHMI]                         NCHAR(1),
+    [DQMPF]                         NCHAR(1),
+    [DTBED]                         NCHAR(1),
+    [DTAUF]                         NCHAR(1),
+    [DERZK]                         NCHAR(1),
+    [DLFPL]                         NCHAR(1),
+    [DLOCK]                         NCHAR(1),
+    [AUPRF]                         NVARCHAR(10),
+    [CrossPlantStatus]              NVARCHAR(25),
+    [SPRAS]                         NCHAR(1),
+    [t_applicationId]               VARCHAR(32),
+    [t_jobId]                       VARCHAR(36),
+    [t_jobDtm]                      DATETIME,
+    [t_lastActionCd]                VARCHAR(1),
+    [t_jobBy]                       NVARCHAR(128),
+    CONSTRAINT [PK_dim_MaterialStatus] PRIMARY KEY NONCLUSTERED ([MMSTA]) NOT ENFORCED
+)
+    WITH
+        (
+        DISTRIBUTION = REPLICATE, HEAP )
+GO
---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_MaterialStatus.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [edw].[vw_MaterialStatus]
+AS
+SELECT
+     T.[MMSTA]
+    ,T.[DEINK]
+    ,T.[DSTLK]
+    ,T.[DSTLP]
+    ,T.[DAPLA]
+    ,T.[DPBED]
+    ,T.[DDISP]
+    ,T.[DFAPO]
+    ,T.[DFAKO]
+    ,T.[DINST]
+    ,T.[DBEST]
+    ,T.[DPROG]
+    ,T.[DFHMI]
+    ,T.[DQMPF]
+    ,T.[DTBED]
+    ,T.[DTAUF]
+    ,T.[DERZK]
+    ,T.[DLFPL]
+    ,T.[DLOCK]
+    ,T.[AUPRF]
+    ,TT.[MTSTB] AS [CrossPlantStatus]
+    ,TT.[SPRAS]
+    ,T.[t_applicationId]
+FROM
+    [base_s4h_cax].[T141] T
+LEFT JOIN
+    [base_s4h_cax].[T141T] TT
+    ON
+        TT.MMSTA = T.MMSTA
+WHERE
+    TT.[SPRAS] = 'E'
