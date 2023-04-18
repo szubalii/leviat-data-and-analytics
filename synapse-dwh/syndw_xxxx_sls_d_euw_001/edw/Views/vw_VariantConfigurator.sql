b7347c8015b402f56343cdc61d99e0f6a08e688c
Tamara Dorodnova <tdorodnova@crh.com>
2023-04-18 18:36:35 +0400

    Move pivot query to edw

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_VariantConfigurator.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_VariantConfigurator.sql
@@ -1,11 +1,12 @@
 ﻿CREATE VIEW [edw].[vw_VariantConfigurator]
 AS
-SELECT
+WITH VC AS(
+SELECT 
     SalesDocument,
     SalesDocumentItem,
     ProductID,
     ProductExternalID,
-    substring(CharacteristicName,5,LEN(CharacteristicName)-4) AS CharacteristicName,
+    RIGHT(CharacteristicName,LEN(CharacteristicName)-4) AS CharacteristicName,
     CASE
       WHEN CharValue<>CharValueDescription THEN CONCAT(CharValue,'_',CharValueDescription)
       WHEN CharValue IS NULL AND CharValueDescription IS NOT NULL THEN CharValueDescription
@@ -16,3 +17,108 @@ SELECT
     [t_applicationId]
 FROM
     [base_s4h_cax].[Z_C_VariantConfig_active]
+)
+SELECT 
+     [SalesDocument]
+    ,[SalesDocumentItem]
+    ,[ProductID]
+    ,[ProductExternalID]
+    ,[SO_ITEM_PROD_HIERARCHY] 
+    ,[SO_ITEM_PROD_HIERARCHY1]
+    ,[HM_LENGTH]
+    ,[HM_MATERIAL]
+    ,[HM_PROFILE]
+    ,[HTA_ANCHOR]
+    ,[HTA_FILLER]
+    ,[HTA_LENGTH]
+    ,[HTA_MATERIAL]
+    ,[HTA_PROFILE]
+    ,[HZA_ANCHOR]
+    ,[HZA_PROFILE]
+    ,[HZA_PS_ANCHOR]
+    ,[HZA_PS_MATERIAL]
+    ,[HZA_PS_PROFILE]
+    ,[HDB_DIAM]
+    ,[HDB_HEIGHT]
+    ,[HDB_LENGTH]
+    ,[HDB_STUD_TYPE]
+    ,[HDB_STUDQTY]
+    ,[HDB_TYPE]
+    ,[HIT_CON_COV]
+    ,[HIT_CSB_QTY]
+    ,[HIT_HEIGHT]
+    ,[HIT_MVX_ES]
+    ,[HIT_TB_QTY]
+    ,[HIT_TYPE]
+    ,[HIT_WIDTH]
+    ,[HDBZ_STUD_DIAM]
+    ,[HDBZ_STUD_LENGTH]
+    ,[HIT_QS_DIAM]
+    ,[HIT_QS_QTY]
+    ,[HIT_QS_TYPE]
+    ,[HIT_VARRIANT]
+    ,[HBS05_BAR_DESIGN]
+    ,[HDBZ_CC_BOTTOM]
+    ,[HDBZ_CC_TOP]
+    ,[HDBZ_DIAM]
+    ,[HDBZ_HEIGHT]
+    ,[HIT_STUFFE]
+    ,[t_applicationId]
+FROM
+    (SELECT
+         [SalesDocument]
+        ,[SalesDocumentItem]
+        ,[ProductID]
+        ,[ProductExternalID]
+        ,[CharacteristicName]
+        ,[CharacteristicDescription] 
+        ,[t_applicationId]
+    FROM
+        VC) VC2
+PIVOT
+(
+MAX([CharacteristicDescription])
+FOR [CharacteristicName] 
+IN (
+     [SO_ITEM_PROD_HIERARCHY] 
+    ,[SO_ITEM_PROD_HIERARCHY1]
+    ,[HM_LENGTH]
+    ,[HM_MATERIAL]
+    ,[HM_PROFILE]
+    ,[HTA_ANCHOR]
+    ,[HTA_FILLER]
+    ,[HTA_LENGTH]
+    ,[HTA_MATERIAL]
+    ,[HTA_PROFILE]
+    ,[HZA_ANCHOR]
+    ,[HZA_PROFILE]
+    ,[HZA_PS_ANCHOR]
+    ,[HZA_PS_MATERIAL]
+    ,[HZA_PS_PROFILE]
+    ,[HDB_DIAM]
+    ,[HDB_HEIGHT]
+    ,[HDB_LENGTH]
+    ,[HDB_STUD_TYPE]
+    ,[HDB_STUDQTY]
+    ,[HDB_TYPE]
+    ,[HIT_CON_COV]
+    ,[HIT_CSB_QTY]
+    ,[HIT_HEIGHT]
+    ,[HIT_MVX_ES]
+    ,[HIT_TB_QTY]
+    ,[HIT_TYPE]
+    ,[HIT_WIDTH]
+    ,[HDBZ_STUD_DIAM]
+    ,[HDBZ_STUD_LENGTH]
+    ,[HIT_QS_DIAM]
+    ,[HIT_QS_QTY]
+    ,[HIT_QS_TYPE]
+    ,[HIT_VARRIANT]
+    ,[HBS05_BAR_DESIGN]
+    ,[HDBZ_CC_BOTTOM]
+    ,[HDBZ_CC_TOP]
+    ,[HDBZ_DIAM]
+    ,[HDBZ_HEIGHT]
+    ,[HIT_STUFFE]
+    )
+) AS PVC
