66342e586d4eb8a88d565f8017da8d3d456d2a84
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-05-01 18:44:11 +0400

    Add mappings

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/Classification.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [map_General].[Classification]
+( 
+    [MaterialGroupName] NVARCHAR (64)   NOT NULL,
+    [Classification]    NVARCHAR(8)     NOT NULL,
+    ,CONSTRAINT [PK_map_Classification] PRIMARY KEY NONCLUSTERED ([MaterialGroupName]) NOT ENFORCED
+)
+WITH
+(
+    DISTRIBUTION = REPLICATE,
+    HEAP
+)
+GO
---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/ECLassCodes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [map_General].[EClassCodes]
+( 
+    [MaterialGroupID]           NVARCHAR (16)  NOT NULL,
+    [EClassCode]                NVARCHAR(8)    NOT NULL,
+    [EClassCategory]            NVARCHAR(64),
+    [EClassCategoryDescription] NVARCHAR(128),
+    ,CONSTRAINT [PK_map_EClassCodes] PRIMARY KEY NONCLUSTERED ([MaterialGroupID]) NOT ENFORCED
+)
+WITH
+(
+    DISTRIBUTION = REPLICATE,
+    HEAP
+)
+GO
