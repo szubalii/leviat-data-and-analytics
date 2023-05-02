c2684a3fbec5b5c2a82e5c1e8f09b9cce8e24d0d
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-05-02 18:34:20 +0400

    Added sys fields to mapping

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/Classification.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/Classification.sql
@@ -1,8 +1,13 @@
 CREATE TABLE [map_General].[Classification]
 ( 
-    [MaterialGroupName] NVARCHAR (64)   NOT NULL,
-    [Classification]    NVARCHAR(8)     NOT NULL,
-    CONSTRAINT [PK_map_Classification] PRIMARY KEY NONCLUSTERED ([MaterialGroupName]) NOT ENFORCED
+    [MaterialGroupName] NVARCHAR (64)   NOT NULL
+    ,[Classification]    NVARCHAR(8)     NOT NULL
+    ,[t_applicationId]           VARCHAR   (32) NULL
+    ,[t_jobId]                   VARCHAR   (36) NULL
+    ,[t_jobDtm]                 DATETIME        NULL
+    ,[t_jobBy]                  NVARCHAR  (128) NULL
+    ,[t_filePath]               NVARCHAR (1024) NULL
+    ,CONSTRAINT [PK_map_Classification] PRIMARY KEY NONCLUSTERED ([MaterialGroupName]) NOT ENFORCED
 )
 WITH
 (
--- synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/ECLassCodes.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/map_General/Tables/ECLassCodes.sql
@@ -1,10 +1,15 @@
 CREATE TABLE [map_General].[EClassCodes]
 ( 
-    [MaterialGroupID]           NVARCHAR (16)  NOT NULL,
-    [EClassCode]                NVARCHAR(8)    NOT NULL,
-    [EClassCategory]            NVARCHAR(64),
-    [EClassCategoryDescription] NVARCHAR(128),
-    CONSTRAINT [PK_map_EClassCodes] PRIMARY KEY NONCLUSTERED ([MaterialGroupID]) NOT ENFORCED
+    [MaterialGroupID]           NVARCHAR (16)  NOT NULL
+    ,[EClassCode]                NVARCHAR(8)    NOT NULL
+    ,[EClassCategory]            NVARCHAR(64)
+    ,[EClassCategoryDescription] NVARCHAR(128)
+    ,[t_applicationId]           VARCHAR   (32) NULL
+    ,[t_jobId]                   VARCHAR   (36) NULL
+    ,[t_jobDtm]                 DATETIME        NULL
+    ,[t_jobBy]                  NVARCHAR  (128) NULL
+    ,[t_filePath]               NVARCHAR (1024) NULL
+    ,CONSTRAINT [PK_map_EClassCodes] PRIMARY KEY NONCLUSTERED ([MaterialGroupID]) NOT ENFORCED
 )
 WITH
 (
