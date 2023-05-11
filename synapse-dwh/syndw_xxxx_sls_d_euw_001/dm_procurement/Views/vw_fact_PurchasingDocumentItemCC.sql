719be520a4457e74e7045b85bb2ed1856fba4a06
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-05-11 10:24:20 +0400

    Added view

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_procurement/Views/vw_fact_PurchasingDocumentItemCC.sql
@@ -0,0 +1,35 @@
+CREATE VIEW [dm_procurement].[vw_fact_PurchasingDocumentItemCC]
+AS
+SELECT
+    PDI.[PurchasingDocument],
+    PDI.[PurchasingDocumentItem],
+    PDI.[MaterialID],
+    PDI.[PurchasingDocumentItemText],
+    PDI.[DocumentCurrencyID],
+    PDI.[PlantID],
+    PDI.[CompanyCodeID],
+    PDI.[MaterialGroupID],
+    PDI.[PurchaseContract],
+    PDI.[PurchaseContractItem],
+    PDI.[NetAmount],
+    PDI.[NetAmountTargetCurrency],
+    PDI.[PurchaseOrderQuantity],
+    PDI.[StorageLocationID],
+    PDI.[OrderPriceUnit],
+    PDI.[NetPriceAmount],
+    PDI.[NetPriceQuantity],
+    PDI.[PurchasingDocumentItemCategoryID],
+    PDI.[NextDeliveryOpenQuantity],
+    PDI.[NextDeliveryDate],
+    PDI.[IsCompletelyDelivered],
+    PDI.[OrderQuantityUnit],
+    PDI.[CostCenterID],
+    PDI.[GLAccountID],
+    PDI.[GoodsReceiptQuantity],
+    PDI.[CreationDate],
+    PDI.[TargetCurrency],
+    PDI.[CurrencyType],
+    PDI.[t_applicationId],
+    PDI.[t_extractionDtm]
+FROM
+    [edw].[vw_PurchasingDocumentItemCC]   AS PDI
