e8655f683ae14cc4adac2d9f146ea1128bd71cb7
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-06-07 16:42:34 +0400

    fixed reference

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_ff/Tables/SalesDocumentStatuses.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_ff/Tables/SalesDocumentStatuses.sql
@@ -5,6 +5,11 @@
     [DeliveryStatus]        NVARCHAR(1),
     [InvoiceStatus]         NVARCHAR(1),
     [OrderTypeText]         NVARCHAR(32),
+    [t_applicationId]            VARCHAR(32),
+    [t_jobId]                    VARCHAR(36),
+    [t_jobDtm]                   DATETIME,
+    [t_jobBy]                    NVARCHAR(128) ,
+    [t_filePath]                 NVARCHAR(1024),
     CONSTRAINT [PK_SalesDocumentStatuses] PRIMARY KEY NONCLUSTERED ([Status], [OrderType], [DeliveryStatus],[InvoiceStatus]) NOT ENFORCED,
 )
 WITH (
