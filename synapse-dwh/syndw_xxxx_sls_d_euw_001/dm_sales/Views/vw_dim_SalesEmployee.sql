2f76e443ecfd6e38010a09e901efdf46a31ba716
Solomiya Leshchuk <sleshchuk@crh.com>
2023-06-13 16:28:07 +0300

    changes for redesign

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
@@ -1,19 +1,9 @@
 CREATE VIEW [dm_sales].[vw_dim_SalesEmployee]
 AS 
 SELECT
-        [Personnel]
-      , [FullName]
-FROM (
-      SELECT Personnel, 
-             FullName, 
-             ROW_NUMBER() OVER (PARTITION BY  Personnel ORDER BY cnt DESC) AS row_num
-      FROM (
-           SELECT Personnel, FullName, COUNT(*) as cnt
-           FROM [edw].[vw_dim_SalesEmployee]
-           GROUP BY Personnel, FullName
-           ) total_count
-      ) frequent_value 
-WHERE row_num=1 
+      Employee,
+      FullName
+FROM  [edw].[vw_SalesEmployee]
 
 
 
