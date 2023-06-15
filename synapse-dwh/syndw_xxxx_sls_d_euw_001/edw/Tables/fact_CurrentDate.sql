211dadf3c54a84860527c4ebbc45fc70a9dee8b8
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-06-15 10:45:44 +0400

    fixed table structure

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/fact_CurrentDate.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/fact_CurrentDate.sql
@@ -1,6 +1,10 @@
 CREATE TABLE [edw].[fact_CurrentDate]
 ( 
-    [today]           DATE  NOT NULL
+    [today]                             DATE  NOT NULL,
+    [t_jobId]                           VARCHAR(36),
+    [t_jobDtm]                          DATETIME,
+    [t_lastActionCd]                    VARCHAR(1),
+    [t_jobBy]                           VARCHAR(128)
 )
 WITH
 (
