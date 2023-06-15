c3a2bbf468944365d425d03398845a6201eadaeb
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-06-15 20:03:29 +0400

    Fixed

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_ComCodRegion.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_ComCodRegion.sql
@@ -1,14 +1,25 @@
 CREATE VIEW [dm_sales].[vw_dim_ComCodRegion]
 AS
+WITH Regions AS (
+    SELECT 
+        RegionID,    
+        RegionName,
+        CountryID
+    FROM [edw].[dim_SalesOrganization]
+    GROUP BY
+        RegionID,    
+        RegionName,
+        CountryID
+)
 SELECT
     CC.CompanyCodeID    AS CompanyCodeID,
     SO.RegionID         AS RegionID,
-    SO.RegionID         AS Region,
+    SO.RegionName       AS Region,
     CC.Country          AS CountryID,
     Ctr.Country         AS Country
 FROM [edw].[dim_CompanyCode] CC
 LEFT JOIN [edw].[dim_Country] Ctr
     ON CC.Country = Ctr.CountryID
-LEFT JOIN [edw].[dim_SalesOrganization] SO
+LEFT JOIN Regions SO
     ON CC.Country = SO.CountryID
 WHERE CC.t_applicationId LIKE 's4h%'
