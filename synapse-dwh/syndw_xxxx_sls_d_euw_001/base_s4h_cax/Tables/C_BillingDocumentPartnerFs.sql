f14353f36016be1e3db94a62ba636ff074d70e10
Michiel Pors <you@example.com>
2023-06-15 15:25:53 +0200

    fix for C_BillingDocumentPartnerFs

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/C_BillingDocumentPartnerFs.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/C_BillingDocumentPartnerFs.sql
@@ -3,16 +3,9 @@ CREATE TABLE [base_s4h_cax].[C_BillingDocumentPartnerFs](
 , [SDDocument] nvarchar(10) NOT NULL
 , [PartnerFunction] nvarchar(2) NOT NULL
 , [PartnerFunctionName] nvarchar(20)
-, [AddressID] nvarchar(10)
 , [Customer] nvarchar(10)
 , [Personnel] char(8) collate Latin1_General_100_BIN2
 , [FullName] nvarchar(80)
-, [CityName] nvarchar(40)
-, [StreetName] nvarchar(60)
-, [PostalCode] nvarchar(10)
-, [EmailAddress] nvarchar(241)
-, [PhoneNumber] nvarchar(30)
-, [MobilePhoneNumber] nvarchar(30)
 , [t_applicationId]       VARCHAR (32)
 , [t_jobId]               VARCHAR (36)
 , [t_jobDtm]              DATETIME
