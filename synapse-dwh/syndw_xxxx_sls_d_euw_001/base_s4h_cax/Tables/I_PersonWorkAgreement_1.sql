5d4e048a9c493127a01e572f8de292244263d56c
Solomiya Leshchuk <sleshchuk@crh.com>
2023-06-16 18:03:52 +0300

    updates for table extraction

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_PersonWorkAgreement_1.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_PersonWorkAgreement_1.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [base_s4h_cax].[I_PersonWorkAgreement_1] (
     [MANDT] CHAR(3) NOT NULL  -- Client
-  , [Person] NVARCHAR(10)  --  Business Partner Number
+  , [PersonWorkAgreement] CHAR(8) NOT NULL
+  , [Person] NVARCHAR(10) 
+  , [AuthorizationGroup] NVARCHAR(4) 
   , [PersonFullName] NVARCHAR(80)  -- Full Name
   , [t_applicationId] VARCHAR (32)  -- Application ID
   , [t_jobId] VARCHAR (36)  -- Job ID
@@ -9,7 +11,7 @@ CREATE TABLE [base_s4h_cax].[I_PersonWorkAgreement_1] (
   , [t_extractionDtm] DATETIME  -- Extraction Date Time
   , [t_filePath] NVARCHAR (1024)  -- Filepath
   , CONSTRAINT [PK_I_PersonWorkAgreement_1] PRIMARY KEY NONCLUSTERED(
-      [MANDT]
+      [MANDT], [PersonWorkAgreement]
   ) NOT ENFORCED
 ) WITH (
   HEAP
--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
@@ -1,7 +1,7 @@
 CREATE VIEW [dm_sales].[vw_dim_SalesEmployee]
 AS 
 SELECT
-      Person,
+      PersonWorkAgreement,
       PersonFullName
 FROM  [base_s4h_cax].[I_PersonWorkAgreement_1]
 
