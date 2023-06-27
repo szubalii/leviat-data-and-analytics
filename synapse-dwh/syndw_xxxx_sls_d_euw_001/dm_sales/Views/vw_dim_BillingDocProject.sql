858bc1a7c48c97e259bf61e7880a3019773b0fe7
Solomiya Leshchuk <sleshchuk@crh.com>
2023-06-27 10:32:22 +0300

    changes related to  C_BillingDocumentPartnerFs

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_BillingDocProject.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_BillingDocProject.sql
@@ -3,9 +3,6 @@ CREATE VIEW [dm_sales].[vw_dim_BillingDocProject]
       [Customer]          AS ProjectID
       ,MAX([FullName])    AS Project
       ,[Customer] + '_' + MAX([FullName]) AS ProjectID_Name
-      ,MAX([CityName])    AS CityName
-      ,MAX([StreetName])  AS StreetName
-      ,MAX([PostalCode])  AS PostalCode
       ,[t_applicationId]
       ,[t_extractionDtm]
   FROM [edw].[dim_BillingDocumentPartnerFs]
