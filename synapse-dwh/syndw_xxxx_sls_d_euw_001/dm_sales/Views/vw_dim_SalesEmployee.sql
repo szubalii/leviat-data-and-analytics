fc1d84f011ecb11be0599d41c4475825b1001d0b
Solomiya Leshchuk <sleshchuk@crh.com>
2023-06-29 18:55:37 +0300

    rename to SalesEmployee

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
@@ -1,7 +1,7 @@
 CREATE VIEW [dm_sales].[vw_dim_SalesEmployee]
 AS 
 SELECT
-      PersonWorkAgreement,
+      PersonWorkAgreement AS SalesEmployee,
       PersonFullName
 FROM  [base_s4h_cax].[I_PersonWorkAgreement_1]
 
