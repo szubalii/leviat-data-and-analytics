f6379a93d432125caffabd7e6a1b286e3a12986d
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-07-06 13:38:49 +0400

    Added mapping

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_ff/Tables/ProductHierarchyNode.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [base_ff].[ProductHierarchyNode] (
+    [MaterialID]                    NVARCHAR(64) NOT NULL
+,   [ProductHierarchy]              NVARCHAR(64) NOT NULL
+,   [ProductHierarchyNew]           NVARCHAR(64) NOT NULL
+,   [t_applicationId]    VARCHAR   (32)
+,   [t_jobId]            VARCHAR   (36)
+,   [t_jobDtm]          DATETIME
+,   [t_jobBy]            VARCHAR  (128)
+,   [t_extractionDtm]   DATETIME
+,   [t_filePath]        NVARCHAR (1024)
+,   CONSTRAINT [PK_ProductHierarchyNodeMapping] PRIMARY KEY NONCLUSTERED ([MaterialID,ProductHierarchy]) NOT ENFORCED
+)
+WITH (
+    DISTRIBUTION = REPLICATE,
+    HEAP
+);
