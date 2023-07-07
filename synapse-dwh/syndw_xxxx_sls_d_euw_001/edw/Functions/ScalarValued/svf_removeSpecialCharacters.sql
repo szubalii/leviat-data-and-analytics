72e460b16c75eaad2af5eed2d88bb9fa62f1bd8a
Tamara Dorodnova <tdorodnova@crh.com>
2023-07-07 14:31:22 +0400

    add function svf_removeSpecialCharacters

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_removeSpecialCharacters.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [edw].[svf_removeSpecialCharacters](
+    @inputString NVARCHAR(255),
+    @listOfSpecialChars NVARCHAR(33)
+)
+RETURNS NVARCHAR(255)
+AS
+BEGIN
+DECLARE @outputString NVARCHAR(255)
+    SET @outputString =
+    CASE
+        WHEN PATINDEX('%''%',@listOfSpecialChars)>0
+        THEN
+            REPLACE(TRANSLATE(@outputString,@listOfSpecialChars,REPLICATE(' ',LEN(@listOfSpecialChars)-1)),' ','')
+        ELSE 
+            REPLACE(TRANSLATE(@outputString,@listOfSpecialChars,REPLICATE(' ',LEN(@listOfSpecialChars))),' ','')
+    END
+    RETURN @outputString
+END;
