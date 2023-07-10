f46c5f28424afc9917126e7d39781b39901e170b
Tamara Dorodnova <tdorodnova@crh.com>
2023-07-10 16:19:12 +0400

    add ACDOCA_FinStatemnt

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_ACDOCA_FinStatemnt.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dm_finance].[vw_fact_ACDOCA_FinStatemnt]
+AS
+SELECT
+     [CompanyCodeID]
+    ,[ProfitCenterID]
+    ,[ProfitCenter]
+    ,[GLAccountID]
+    ,[FunctionalAreaID]
+    ,[CurrencyID]
+    ,[SAP_Sales]
+    ,[SAP_COGS]
+    ,[SAP_SalesMargin]
+    ,[FiscalYear]
+    ,[FiscalYearPeriod]
+    ,[t_applicationId]
+    ,[t_extractionDtm]
+FROM
+    [edw].[fact_ACDOCA_FinStatemnt]
---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/fact_ACDOCA_FinStatemnt.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [edw].[fact_ACDOCA_FinStatemnt]
+(
+    [CompanyCodeID]                         NVARCHAR(8),
+    [ProfitCenterID]                        NVARCHAR(20),
+    [ProfitCenter]                          NVARCHAR(40),
+    [GLAccountID]                           NVARCHAR(20),
+    [FunctionalAreaID]                      NVARCHAR(32),
+    [CurrencyID]                            CHAR(5),
+    [SAP_Sales]                             DECIMAL(23, 2),
+    [SAP_COGS]                              DECIMAL(23, 2),
+    [SAP_SalesMargin]                       DECIMAL(23, 2),
+    [FiscalYear]                            CHAR(4),
+    [FiscalYearPeriod]                      CHAR(7),
+    [t_applicationId]                       VARCHAR(32),
+    [t_extractionDtm]                       DATETIME,
+    [t_jobId]                               VARCHAR(36),
+    [t_jobDtm]                              DATETIME,
+    [t_lastActionCd]                        VARCHAR(1),
+    [t_jobBy]                               NVARCHAR(128)
+)
+    WITH
+        (
+        DISTRIBUTION = REPLICATE, HEAP )
+GO
