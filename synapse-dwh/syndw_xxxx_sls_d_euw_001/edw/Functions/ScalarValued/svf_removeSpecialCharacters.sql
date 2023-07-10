3e31fe685cd0601688b948ba28a6db1bd0d6fba6
Tamara Dorodnova <tdorodnova@crh.com>
2023-07-10 12:31:03 +0400

    fix function

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_removeSpecialCharacters.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_removeSpecialCharacters.sql
@@ -1,12 +1,11 @@
 ﻿CREATE FUNCTION [edw].[svf_removeSpecialCharacters](
-    @inputString NVARCHAR(255),
-    @listOfSpecialChars NVARCHAR(33)
+    @inputString NVARCHAR(255)
 )
 RETURNS NVARCHAR(255)
 AS
 BEGIN
     DECLARE @outputString NVARCHAR(255)
     SET @outputString =
-        REPLACE(TRANSLATE(@inputString,@listOfSpecialChars,REPLICATE(' ',LEN(@listOfSpecialChars))),' ','')
+        REPLACE(TRANSLATE(@inputString,'!"#$&''()*+,-./:;<=>?@[\]^`{|}~%“',REPLICATE(' ',LEN('!"#$&''()*+,-./:;<=>?@[\]^`{|}~%“'))),' ','')
     RETURN @outputString
 END;
