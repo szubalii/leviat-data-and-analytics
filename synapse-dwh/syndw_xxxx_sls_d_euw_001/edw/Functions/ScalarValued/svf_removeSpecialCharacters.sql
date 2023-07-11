12fd542ea84e4332b0b0c500c51e3ec382ce2797
Tamara Dorodnova <tdorodnova@crh.com>
2023-07-11 14:47:22 +0400

    add variable

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_removeSpecialCharacters.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_removeSpecialCharacters.sql
@@ -4,8 +4,9 @@
 RETURNS NVARCHAR(255)
 AS
 BEGIN
+    DECLARE @specialChars CHAR(32) = '!"#$&''()*+,-./:;<=>?@[\]^`{|}~%“';
     DECLARE @outputString NVARCHAR(255)
     SET @outputString =
-        REPLACE(TRANSLATE(@inputString,'!"#$&''()*+,-./:;<=>?@[\]^`{|}~%“',REPLICATE(' ',LEN('!"#$&''()*+,-./:;<=>?@[\]^`{|}~%“'))),' ','')
+        REPLACE(TRANSLATE(@inputString,@specialChars,REPLICATE(' ',LEN(@specialChars))),' ','')
     RETURN @outputString
 END;
