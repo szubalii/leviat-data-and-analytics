6a07219da57baab19bea2d80c5dd2ab895d51c86
Solomiya Leshchuk <sleshchuk@crh.com>
2023-07-11 16:38:59 +0300

    added row_number to the nk_ field

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_BillingDocumentItem_axbi.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_BillingDocumentItem_axbi.sql
@@ -870,7 +870,11 @@ BillingDocumentItemBase_axbi_mapped AS (
 */
 
 SELECT 
-        edw.svf_getNaturalKey (BillingDocument,BillingDocumentItem,CT.CurrencyTypeID) as [nk_fact_BillingDocumentItem]
+        CONCAT_WS(
+             '¦',
+             edw.svf_getNaturalKey (BillingDocument,BillingDocumentItem,CT.CurrencyTypeID),
+             row_number() over (partition by BillingDocument order by BillingDocumentItem)
+             ) as nk_fact_BillingDocumentItem
     ,   [BillingDocument]
     ,   [BillingDocumentItem]
     ,   [ReturnItemProcessingType]
@@ -940,7 +944,11 @@ UNION ALL
 */
 
 SELECT 
-        edw.svf_getNaturalKey (BillingDocument,BillingDocumentItem,CT.CurrencyTypeID) AS [nk_fact_BillingDocumentItem]
+        CONCAT_WS(
+             '¦',
+             edw.svf_getNaturalKey (BillingDocument,BillingDocumentItem,CT.CurrencyTypeID),
+             row_number() over (partition by BillingDocument order by BillingDocumentItem)
+             ) as nk_fact_BillingDocumentItem
     ,   [BillingDocument]
     ,   [BillingDocumentItem]
     ,   [ReturnItemProcessingType]
@@ -1006,7 +1014,11 @@ JOIN
 UNION ALL
 
 SELECT 
-        edw.svf_getNaturalKey (ExchangeRateUSD.BillingDocument,ExchangeRateUSD.BillingDocumentItem,CT.CurrencyTypeID) AS [nk_fact_BillingDocumentItem]
+        CONCAT_WS(
+             '¦',
+             edw.svf_getNaturalKey (ExchangeRateUSD.BillingDocument,ExchangeRateUSD.BillingDocumentItem,CT.CurrencyTypeID),
+             row_number() over (partition by ExchangeRateUSD.BillingDocument order by ExchangeRateUSD.BillingDocumentItem)
+             ) as nk_fact_BillingDocumentItem
     ,   ExchangeRateUSD.[BillingDocument]
     ,   ExchangeRateUSD.[BillingDocumentItem]
     ,   [ReturnItemProcessingType]
