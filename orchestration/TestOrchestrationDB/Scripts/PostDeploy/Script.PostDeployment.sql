6f59c2753df5498843984a24e7532891848a58f1
Michiel Pors <you@example.com>
2023-07-11 13:54:07 +0200

    add missing test classes

--- orchestration/TestOrchestrationDB/Scripts/PostDeploy/Script.PostDeployment.sql
+++ orchestration/TestOrchestrationDB/Scripts/PostDeploy/Script.PostDeployment.sql
@@ -1,3 +1,6 @@
 -- This file contains SQL statements that will be executed after the build script.
 
+:r ..\..\TestClasses\DirectoryPath.sql
+:r ..\..\TestClasses\EntityFile.sql
+:r ..\..\TestClasses\LoadTypeEntities.sql
 :r ..\..\TestClasses\ScheduledEntities.sql
