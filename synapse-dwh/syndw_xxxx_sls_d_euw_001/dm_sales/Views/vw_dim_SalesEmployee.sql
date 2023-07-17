bb9c7a8fe1d8fab3c21bff7eb31966bd079c57dc
Michiel Pors <you@example.com>
2023-07-17 13:50:09 +0200

    salesemployee

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SalesEmployee.sql
@@ -1,10 +1,7 @@
 CREATE VIEW [dm_sales].[vw_dim_SalesEmployee]
 AS 
 SELECT
-      PersonWorkAgreement AS SalesEmployee,
-      PersonFullName
-FROM  [base_s4h_cax].[I_PersonWorkAgreement_1]
-
-
-
-
+  PersonWorkAgreement AS SalesEmployeeID,
+  PersonFullName AS SalesEmployee
+FROM
+  [base_s4h_cax].[I_PersonWorkAgreement_1]
