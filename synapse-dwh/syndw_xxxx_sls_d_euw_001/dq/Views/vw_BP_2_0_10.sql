ffe5a8ef7f768cd49cf624afa2044a22ee04db73
Tamara Dorodnova <tdorodnova@crh.com>
2023-07-18 14:40:17 +0400

    add upper

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dq/Views/vw_BP_2_0_10.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dq/Views/vw_BP_2_0_10.sql
@@ -81,7 +81,7 @@ LEFT JOIN
 WHERE
     BP.[BusinessPartnerGrouping] IN ('Z001', 'Z002')
     AND
-    BP.[SearchTerm1] COLLATE SQL_Latin1_General_CP1_CS_AS <> CONCAT_WS(
+    UPPER(BP.[SearchTerm1] COLLATE SQL_Latin1_General_CP1_CS_AS) <> UPPER(CONCAT_WS(
         '',
         LEFT(edw.svf_removeSpecialCharacters(BP.[OrganizationBPName1] COLLATE SQL_Latin1_General_CP1_CS_AS), 5),
-        LEFT(edw.svf_removeSpecialCharacters(A.[CityName]), 5))
+        LEFT(edw.svf_removeSpecialCharacters(A.[CityName]), 5)))
