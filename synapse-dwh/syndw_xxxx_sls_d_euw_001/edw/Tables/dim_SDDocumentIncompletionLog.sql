7bd67bae8f16e03eebc7983b846cb4e42089adce
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-07-20 14:41:41 +0400

    Fixed extraction

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_SDDocumentIncompletionLog.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_SDDocumentIncompletionLog.sql
@@ -1,10 +1,15 @@
 CREATE TABLE [edw].[dim_SDDocumentIncompletionLog]
 (
     
-    [SDDocument]        NVARCHAR(10) NOT NULL
+    [SDDocument]          NVARCHAR(10) NOT NULL
     , [SDDocumentItem]    CHAR(6) NOT NULL
     , [ScheduleLine]      CHAR(4) NOT NULL
     , [FieldText]         NVARCHAR(55)
+    , [t_applicationId]   VARCHAR(32)
+    , [t_jobId]           VARCHAR(36)
+    , [t_jobDtm]          DATETIME   
+    , [t_lastActionCd]    VARCHAR(1)
+    , [t_jobBy]           NVARCHAR(128)
     , CONSTRAINT [PK_dim_SDDocumentIncompletionLog] PRIMARY KEY NONCLUSTERED(
           [SDDocument]
         , [SDDocumentItem]
