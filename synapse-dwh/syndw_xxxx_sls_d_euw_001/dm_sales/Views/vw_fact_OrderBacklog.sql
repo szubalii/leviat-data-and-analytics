0e88e493bb3b478e2c6f297f3a775f94f56ed85a
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-07-21 11:12:36 +0400

    Changed OrderBacklog

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_fact_OrderBacklog.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_fact_OrderBacklog.sql
@@ -164,6 +164,5 @@ LEFT JOIN doc_delivery
 LEFT JOIN [edw].[dim_SDDocumentIncompletionLog] IL
     ON doc.SalesDocument = IL.SDDocument    COLLATE DATABASE_DEFAULT
         AND doc.SalesDocumentItem = IL.SDDocumentItem   COLLATE DATABASE_DEFAULT
-        AND SDSL.ScheduleLine = IL.ScheduleLine COLLATE DATABASE_DEFAULT
 
 WHERE doc.SDDocumentCategory = 'C'
