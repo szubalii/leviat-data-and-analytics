eb1863cdf4848c280a218a2be7e31e60f60250c2
Fred Bouw <fbouw@crh.com>
2023-08-09 13:32:19 +0200

    Added comments

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/Scripts/Currency-Conversion/SalesDoc_s4h-CurrencyConversion.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/Scripts/Currency-Conversion/SalesDoc_s4h-CurrencyConversion.sql
@@ -1,4 +1,4 @@
-Samples examples on CAP-100.
+--Samples examples on CAP-100.
 SELECT CurrencyType, CurrencyID, ExchangeRate, TransactionCurrencyID, NetAmount
 
 FROM edw.vw_SalesDocumentItem_s4h A
@@ -7,14 +7,14 @@ WHERE A.SalesDocument = '0020088135' --Different Currencies.
 WHERE A.SalesDocument = '0020044818' --Same Currencies.
 
 
-Transaction Currency. We should not convert the values, and the ExchangeRate should be 1.
+--Transaction Currency. We should not convert the values, and the ExchangeRate should be 1.
 SELECT DISTINCT(ExchangeRate)
 
 FROM edw.vw_SalesDocumentItem_s4h A
 
 WHERE CurrencyType = 'Transaction Currency'
 
-Local Currency. We shouldn't convert if the currencies match, if they don't they should be converted.
+--Local Currency. We shouldn't convert if the currencies match, if they don't they should be converted.
 SELECT DISTINCT(ExchangeRate)
 
 FROM edw.vw_SalesDocumentItem_s4h A
@@ -44,11 +44,11 @@ FROM LC
 WHERE ExchangeRateResult <> ExchangeRateCheck
 
 
-Group Currency EUR. 
-For cases where the Transaction Currency is not equal to the Currency of the Company we expect to have an exchange rate that equals the result of the 
-PriceDetnExchangeRate and the ExchangeRate from CCR.
+--Group Currency EUR. 
+--For cases where the Transaction Currency is not equal to the Currency of the Company we expect to have an exchange rate that equals the result of the 
+--PriceDetnExchangeRate and the ExchangeRate from CCR.
 
-For cases where the transaction currency is equal to the currency of the compant we expect to have an exchange rate that equals the ExchangeRate from CCR.
+--For cases where the transaction currency is equal to the currency of the compant we expect to have an exchange rate that equals the ExchangeRate from CCR.
 , B AS(
 SELECT
 A.SalesDocument, A.SalesDocumentItem, A.TransactionCurrencyID, A.CurrencyID, A.ExchangeRate as ExchangeRateResult, CCR.ExchangeRate, A.PriceDetnExchangeRate, 
@@ -85,11 +85,11 @@ FROM B
 WHERE ExchangeRateCheck <> ExchangeRateResult
 
 
-Group Currency USD
-For cases where the Transaction Currency is not equal to the Currency of the Company we expect to have an exchange rate that equals the result of the 
-PriceDetnExchangeRate and the ExchangeRate from CCR.
+--Group Currency USD
+--For cases where the Transaction Currency is not equal to the Currency of the Company we expect to have an exchange rate that equals the result of the 
+--PriceDetnExchangeRate and the ExchangeRate from CCR.
 
-For cases where the transaction currency is equal to the currency of the compant we expect to have an exchange rate that equals the ExchangeRate from CCR.
+--For cases where the transaction currency is equal to the currency of the compant we expect to have an exchange rate that equals the ExchangeRate from CCR.
 , B AS(
 SELECT
 A.SalesDocument, A.SalesDocumentItem, A.TransactionCurrencyID, A.CurrencyID, A.ExchangeRate as ExchangeRateResult, CCR.ExchangeRate, A.PriceDetnExchangeRate, 
