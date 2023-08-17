ad57aac0968036120a0b6c88ffc9761f2649cc6c
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-08-17 12:05:57 +0400

    Revert changes to date fields

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_BusinessPartnerAddress.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_dq/Views/vw_dim_BusinessPartnerAddress.sql
@@ -3,23 +3,7 @@ AS
 SELECT
     BUSINESSPARTNER     AS BusinessPartner
     , ADDRESSNUMBER     AS AddressNumber
-    , DATETIMEFROMPARTS(
-        ValidityStartDate / 10000000000
-        ,ValidityStartDate % 10000000000 / 100000000
-        ,ValidityStartDate % 100000000 / 1000000
-        ,ValidityStartDate % 1000000 / 10000
-        ,ValidityStartDate % 10000 / 100
-        ,ValidityStartDate % 100
-        ,0
-    )                   AS ValidityStartDate
-    , DATETIMEFROMPARTS(
-        ValidityEndDate / 10000000000
-        ,ValidityEndDate % 10000000000 / 100000000
-        ,ValidityEndDate % 100000000 / 1000000
-        ,ValidityEndDate % 1000000 / 10000
-        ,ValidityEndDate % 10000 / 100
-        ,ValidityEndDate % 100
-        ,0
-    )                   AS ValidityEndDate
+    , ValidityStartDate
+    , ValidityEndDate
 FROM
     [base_s4h_cax].[I_BusinessPartnerAddress]
