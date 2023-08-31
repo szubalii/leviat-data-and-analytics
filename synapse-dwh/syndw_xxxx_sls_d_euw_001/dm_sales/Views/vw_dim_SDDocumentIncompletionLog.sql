fe8048d1c56d341670124bbe49b688e047b766fb
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-08-31 15:29:39 +0400

    fixed view

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SDDocumentIncompletionLog.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_SDDocumentIncompletionLog.sql
@@ -4,7 +4,7 @@ SELECT
     [MANDT]
     , [SDDocument]
     , [SDDocumentItem]
-    , svf_get2PartNaturalKey(doc.SalesDocument, doc.SalesDocumentItem)  AS nk_SDDocumentItem
+    , svf_get2PartNaturalKey(SalesDocument, SalesDocumentItem)  AS nk_SDDocumentItem
     , [ScheduleLine]
     , [PartnerFunction]
     , [SDDocumentTextID]
