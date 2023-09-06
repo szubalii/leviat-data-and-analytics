df6b60ea5aff0f94355ab064afa65d39f66ff9fb
Michiel Pors <you@example.com>
2023-09-06 16:07:50 +0200

    include join to rollname

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_SDDocumentIncompletionLog.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_SDDocumentIncompletionLog.sql
@@ -1,17 +1,23 @@
 CREATE VIEW [edw].[vw_SDDocumentIncompletionLog]
 AS
+
 SELECT
-    IL.[MANDT]
-    , IL.[SDDocument]
-    , IL.[SDDocumentItem]
-    , IL.[ScheduleLine]
-    , IL.[PartnerFunction]
-    , IL.[SDDocumentTextID]
-    , IL.[SDDocumentTable]
-    , IL.[SDDocumentTableField] 
-    , LT.[ABAPDATAELEMENTHEADING]          AS FieldName
-    , LT.[ABAPDATAELEMENTDESCRIPTION]      AS FieldDescription
+  IL.[MANDT]
+, IL.[SDDocument]
+, IL.[SDDocumentItem]
+, IL.[ScheduleLine]
+, IL.[PartnerFunction]
+, IL.[SDDocumentTextID]
+, IL.[SDDocumentTable]
+, IL.[SDDocumentTableField]
+, LT.[ABAPDATAELEMENTHEADING]          AS FieldName
+, LT.[ABAPDATAELEMENTDESCRIPTION]      AS FieldDescription
 FROM [base_s4h_cax].[I_SDDocumentIncompletionLog] IL
+LEFT JOIN [base_s4h_cax].[RollName] RN
+  ON
+    RN.TABNAME = IL.SDDocumentTable
+    AND
+    RN.FIELDNAME = IL.SDDocumentTableField
 LEFT JOIN [base_s4h_cax].[I_DataElementLabelText] LT
-    ON IL.SDDocumentTableField = LT.ABAPDATAELEMENT
+  ON LT.ABAPDATAELEMENT = RN.ROLLNAME
 WHERE IL.SDDocument NOT LIKE '001%'
