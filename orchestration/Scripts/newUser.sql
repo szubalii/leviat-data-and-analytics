d8d4b8645ec17a413cd0c8ce9f6cb1667ae78109
Michiel Pors <you@example.com>
2023-09-11 09:23:03 +0200

    new user script

--- orchestration/Scripts/newUser.sql
+++ orchestration/Scripts/newUser.sql
@@ -1,10 +1,6 @@
-Use master
-CREATE LOGIN [<alias@domain.com>] FROM EXTERNAL PROVIDER
-GO
-
-use [sqldb-xxxx-sls-d-euw-001]
-CREATE USER [<alias@domain.com>] FROM LOGIN [<alias@domain.com>];
-ALTER ROLE [db_datawriter] ADD MEMBER [<alias@domain.com>];
+USE [sqldb-xxxx-sls-d-euw-001]
+CREATE USER [<alias@domain.com>] FROM EXTERNAL PROVIDER;
 ALTER ROLE [db_datareader] ADD MEMBER [<alias@domain.com>];
 ALTER ROLE [db_developer] ADD MEMBER [<alias@domain.com>];
+ALTER ROLE [db_datawriter] ADD MEMBER [<alias@domain.com>];
 GO
