b7d75c168a6b5a7ffeb0743dae5d4af10daee5e3
Michiel Pors <you@example.com>
2023-09-12 15:41:58 +0200

    syntax

--- xu-config/src/getChangedExtractions.ps1
+++ xu-config/src/getChangedExtractions.ps1
@@ -37,6 +37,6 @@ $extractions
 ForEach ($extraction in $extractions) {
   $extractionName = $env:SYSTEM_PULLREQUEST_PULLREQUESTID + '_' + $extraction
   $params = '-s localhost -p 8065 -n '+$extractionName
-  $Command = "C:\Program Files\XtractUniversal\xu.exe"
+  $Command = "C:\'Program Files'\XtractUniversal\xu.exe"
   Invoke-Expression -Command "$Command $params"
 }
