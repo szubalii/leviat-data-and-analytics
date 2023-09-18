9ef98716fa50b127227457b592bf8a9214123095
Michiel Pors <you@example.com>
2023-09-18 11:37:11 +0200

    test delta extractions

--- xu-config/src/runExtractions.js
+++ xu-config/src/runExtractions.js
@@ -2,11 +2,12 @@ const { exec } = require('child_process');
 // const xuPrefix;
 const dir = __dirname + '/../extractions';
 
-
+// Run comma separated list of extraction names
 function runExtractions(xuPrefix, extractionNames) {
   extractionNames.split(',').forEach((extractionName) => runExtraction(xuPrefix, extractionName));
 }
 
+// Run single extraction
 function runExtraction(xuPrefix, extractionName) {
   let command = getExtractionCommand(xuPrefix, extractionName);
 
@@ -15,15 +16,18 @@ function runExtraction(xuPrefix, extractionName) {
   });
 }
 
+// Get the Xtract Universal command to run a specific extraction
 function getExtractionCommand(xuPrefix, extractionName) {
   let extractionType = getExtractionType(extractionName);
   let command = `C:\\"Program Files"\\XtractUniversal\\xu.exe`;
   let params = '-s localhost -p 8065 -n ' + extractionName + ' -o "preview=True"';
 
   switch (extractionType) {
+  // Extract 10.000 rows only in case of Table extraction type
     case 'Table':
       params += ' -o "rows=10000"';
       break;
+  // Add subscription suffix in case of ODP extraction type
     case 'ODP':
       params += ' -o "subscriptionSuffix=' + xuPrefix + '"';
       break;
@@ -34,12 +38,14 @@ function getExtractionCommand(xuPrefix, extractionName) {
   return command + ' ' + params;
 }
 
+// Read the general.json extraction file and return the extraction type (ODP or Table) 
 function getExtractionType(extractionName) {
   let generalConfigFile = readExtractionConfigFile(extractionName, 'general');
 
   return generalConfigFile.type;
 }
 
+// Read an extraction config file for a given extraction
 function readExtractionConfigFile(extractionName, configFileName) {
 
   let filePath = dir + '/' + extractionName + '/' + configFileName + '.json';
