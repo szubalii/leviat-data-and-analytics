0ce4ee1a2c864c1ea05baacf3caa1d46862643b5
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-09-26 19:00:30 +0400

    Fixed fields in views

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_procurement/Views/vw_dim_OptVim1Head.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_procurement/Views/vw_dim_OptVim1Head.sql
@@ -5,6 +5,18 @@ SELECT
     ,DocumentType
     ,CreationDate
     ,AccountingDocumentID
-    ,PurchasingDocumentID
+    ,NULL AS PurchasingDocumentID
+    ,[Object Type]
+    ,[Document Status]
+    ,NotFirstPass
+    ,AccountingDocumentTypeID
+    ,FiscalYear
+    ,DocumentDate
+    ,PostingDat
+    ,SupplierID
+    ,ReferenceID
+    ,CurrencyID
+    ,HeaderTex
+    ,CountryI
 FROM
     [edw].[vw_OptVim1Head]
--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_OptVim1Head.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_OptVim1Head.sql
@@ -6,7 +6,18 @@ SELECT
     ,[DOCTYPE]              AS DocumentType
     ,[INDEX_DATE]           AS CreationDate
     ,[BELNR]                AS AccountingDocumentID
-    ,[EBELN]                AS PurchasingDocumentID
+    ,[BUS_OBJTYPE]          AS [Object Type]
+    ,[STATUS]               AS [Document Status]
+    ,[NOFIRSTPASS]          AS NotFirstPass
+    ,[BLART]                AS AccountingDocumentTypeID
+    ,[GJAHR]                AS FiscalYear
+    ,[BLDAT]                AS DocumentDate
+    ,[BUDAT]                AS PostingDat
+    ,[LIFNR]                AS SupplierID
+    ,[XBLNR]                AS ReferenceID
+    ,[WAERS]                AS CurrencyID
+    ,[BKTXT]                AS HeaderTex
+    ,[LAND1]                AS CountryI
 FROM
     [base_s4h_cax].[Opt_Vim_1Head]
 --WHERE
