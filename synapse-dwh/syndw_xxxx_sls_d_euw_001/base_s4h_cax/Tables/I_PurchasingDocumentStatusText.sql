017336fa2ff673f00ce4dda30b240ab2a9493970
Michiel Pors <you@example.com>
2023-10-04 09:27:46 +0200

    remove collations from other Purchasing tables

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_PurchasingDocumentStatusText.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_PurchasingDocumentStatusText.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [base_s4h_cax].[I_PurchasingDocumentStatusText]
 -- MM: Status Description of Purchasing Document
 (
-    [PurchasingDocumentStatus]     nvarchar(2) collate Latin1_General_100_BIN2 NOT NULL,
-    [Language]                     nchar(1) collate Latin1_General_100_BIN2    NOT NULL,
-    [PurchasingDocumentStatusName] nvarchar(60) collate Latin1_General_100_BIN2,
+    [PurchasingDocumentStatus]     nvarchar(2) NOT NULL, -- collate Latin1_General_100_BIN2 NOT NULL,
+    [Language]                     nchar(1) NOT NULL, -- collate Latin1_General_100_BIN2    NOT NULL,
+    [PurchasingDocumentStatusName] nvarchar(60), -- collate Latin1_General_100_BIN2,
     [t_applicationId]              VARCHAR(32),
     [t_jobId]                      VARCHAR(36),
     [t_jobDtm]                     DATETIME,
