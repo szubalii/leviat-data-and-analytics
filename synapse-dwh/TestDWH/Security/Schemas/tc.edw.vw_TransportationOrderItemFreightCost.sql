183512a5d5903b1d46eb251ef74bf5cdf4c2f9c3
Michiel Pors <you@example.com>
2023-10-05 14:13:33 +0200

    remove freight test cases

--- synapse-dwh/TestDWH/Security/Schemas/tc.edw.vw_TransportationOrderItemFreightCost.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA [tc.edw.vw_TransportationOrderItemFreightCost];
-GO
-
--- EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'edw.vw_TransportationOrderItemFreightCost';
--- GO
