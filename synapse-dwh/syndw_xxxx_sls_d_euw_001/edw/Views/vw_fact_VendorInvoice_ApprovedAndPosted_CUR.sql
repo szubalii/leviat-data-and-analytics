e76c6b10616c7f2fcd6185d24cae3f049af10f0e
Michiel Pors <you@example.com>
2023-10-06 11:42:29 +0200

    Convert collation

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_CUR.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_CUR.sql
@@ -54,13 +54,13 @@ FROM
 LEFT JOIN
   [edw].[vw_CurrencyConversionRate] CCR_EUR
     ON
-      CCR_EUR.SourceCurrency = VIM.HDR1_CurrencyID
+      CCR_EUR.SourceCurrency COLLATE DATABASE_DEFAULT = VIM.HDR1_CurrencyID
       AND
       CCR_EUR.CurrencyTypeID = '30'
 LEFT JOIN
   [edw].[vw_CurrencyConversionRate] CCR_USD
     ON
-      CCR_USD.SourceCurrency = VIM.HDR1_CurrencyID
+      CCR_USD.SourceCurrency COLLATE DATABASE_DEFAULT = VIM.HDR1_CurrencyID
       AND
       CCR_USD.CurrencyTypeID = '40'
 LEFT JOIN
