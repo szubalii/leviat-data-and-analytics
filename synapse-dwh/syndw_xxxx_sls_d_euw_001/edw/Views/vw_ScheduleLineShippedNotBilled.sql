f239e8a57a0029a89f5fa596b19abe24bd49cc7f
Michiel Pors <you@example.com>
2023-10-09 14:00:37 +0200

    add [edw].[vw_fact_ScheduleLineStatus]

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ScheduleLineShippedNotBilled.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_ScheduleLineShippedNotBilled.sql
@@ -1,48 +1,54 @@
-CREATE VIEW [edw].[vw_ScheduleLineShippedNotBilled]
-AS
-SELECT 
-        SLS.[nk_fact_SalesDocumentItem],
-        SLS.[SalesDocumentID],      
-        SLS.[SalesDocumentItem],
-        CONVERT (date, GETDATE()) AS [ReportDate],
-        SLS.[SalesDocumentTypeID],
-        SLS.[SDDocumentRejectionStatusID], 
-        SLS.[IsUnconfirmedDelivery],
-        SLS.[CurrencyTypeID],
-        SLS.[ScheduleLine],
-        SLS.[SalesDocumentOrderType],
-        SLS.[OrderStatus],
-        SLS.[ItemOrderStatus],
-        SLS.[SLDeliveryStatus],
-        SLS.[SLInvoicedStatus],
-        SLS.[SLStatus],
-        SLS.[CreationDate],
-        SLS.[RequestedDeliveryDate],
-        SLS.[ConfirmedDeliveryDate],
-        SLS.[SDI_ODB_LatestActualGoodsMovmtDate],
-        SLS.[DelivBlockReasonForSchedLine],
-        SLS.[LoadingDate],
-        SLS.[ConfirmedQty],
-        SLS.[TotalOrderQty],
-        SLS.[TotalDelivered],
-        SLS.[SDSLOrderQtyRunningSum],
-        SLS.[ValueConfirmedQuantity],
-        SLS.[CurrencyID],
-        SLS.[BillingQuantity],
-        SLS.[ShippingConditionID],
-        SLS.[ScheduleLineCategory],
-        SLS.[NetAmount],
-        SLS.[OpenDeliveryValue],
-        SLS.[ClosedDeliveryValue],
-        SLS.[OpenInvoicedValue],
-        SLS.[ClosedInvoicedValue],
-        SLS.[PricePerUnit],
-        SLS.[InScope],
-        ODI.[HDR_ActualGoodsMovementDate],
-        ODI.[t_applicationId],
-        ODI.[t_extractionDtm]
-FROM [dm_sales].[vw_fact_ScheduleLineStatus] SLS
-LEFT JOIN [edw].[fact_OutboundDeliveryItem] ODI
-       ON SLS.SalesDocumentID = ODI.ReferenceSDDocument
-       AND SLS.SalesDocumentItem = ODI.ReferenceSDDocumentItem
-WHERE SLS.SLDeliveryStatus IN ('P', 'C') AND SLS.SLInvoicedStatus IN ('P', 'N')
+CREATE VIEW [edw].[vw_ScheduleLineShippedNotBilled] AS
+SELECT
+  SLS.[nk_fact_SalesDocumentItem],
+  SLS.[SalesDocumentID],
+  SLS.[SalesDocumentItem],
+  CONVERT (date, GETDATE()) AS [ReportDate],
+  SLS.[SalesDocumentTypeID],
+  SLS.[SDDocumentRejectionStatusID],
+  SLS.[IsUnconfirmedDelivery],
+  SLS.[CurrencyTypeID],
+  SLS.[ScheduleLine],
+  SLS.[SalesDocumentOrderType],
+  SLS.[OrderStatus],
+  SLS.[ItemOrderStatus],
+  SLS.[SLDeliveryStatus],
+  SLS.[SLInvoicedStatus],
+  SLS.[SLStatus],
+  SLS.[CreationDate],
+  SLS.[RequestedDeliveryDate],
+  SLS.[ConfirmedDeliveryDate],
+  SLS.[SDI_ODB_LatestActualGoodsMovmtDate],
+  SLS.[DelivBlockReasonForSchedLine],
+  SLS.[LoadingDate],
+  SLS.[ConfirmedQty],
+  SLS.[TotalOrderQty],
+  SLS.[TotalDelivered],
+  SLS.[SDSLOrderQtyRunningSum],
+  SLS.[ValueConfirmedQuantity],
+  SLS.[CurrencyID],
+  SLS.[BillingQuantity],
+  SLS.[ShippingConditionID],
+  SLS.[ScheduleLineCategory],
+  SLS.[NetAmount],
+  SLS.[OpenDeliveryValue],
+  SLS.[ClosedDeliveryValue],
+  SLS.[OpenInvoicedValue],
+  SLS.[ClosedInvoicedValue],
+  SLS.[PricePerUnit],
+  SLS.[InScope],
+  ODI.[HDR_ActualGoodsMovementDate],
+  ODI.[t_applicationId],
+  ODI.[t_extractionDtm]
+FROM
+  [edw].[vw_fact_ScheduleLineStatus] SLS
+LEFT JOIN
+  [edw].[fact_OutboundDeliveryItem] ODI
+  ON
+    SLS.SalesDocumentID = ODI.ReferenceSDDocument
+    AND
+    SLS.SalesDocumentItem = ODI.ReferenceSDDocumentItem
+WHERE
+  SLS.SLDeliveryStatus IN ('P', 'C')
+  AND
+  SLS.SLInvoicedStatus IN ('P', 'N')
