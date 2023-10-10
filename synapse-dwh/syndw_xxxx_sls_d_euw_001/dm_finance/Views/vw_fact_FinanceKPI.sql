5cc4de77ec58f7ff1557bb6b14b273b5ce683cd1
Michiel Pors <you@example.com>
2023-10-10 17:12:27 +0200

    data mart update

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
@@ -1,7 +1,35 @@
 CREATE VIEW [dm_finance].[vw_fact_FinanceKPI] AS
 SELECT
-
+  CompanyCodeID,
+  FiscalYear,
+  FiscalPeriod,
+  FiscalYearPeriod,
+  KPIName
+  KPIValue
 FROM
-  [edw].[vw_fact_ACDOCA_EPM_OtherCOS]
+  [edw].[vw_fact_ACDOCA_EPM_Sales_agg_unpvt]
 
+UNION
+
+SELECT
+  CompanyCodeID,
+  FiscalYear,
+  FiscalPeriod,
+  FiscalYearPeriod,
+  KPIName
+  KPIValue
+FROM
+  [edw].[vw_fact_ManualJournalEntriesCount]
+
+UNION
+
+SELECT
+  CompanyCodeID,
+  FiscalYear,
+  FiscalPeriod,
+  FiscalYearPeriod,
+  KPIName
+  KPIValue
+FROM
+  [edw].[vw_fact_OpenInvoiced]
 
