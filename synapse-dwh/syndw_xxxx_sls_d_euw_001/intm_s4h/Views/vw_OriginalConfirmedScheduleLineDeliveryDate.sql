64233df6ccde5de185cb72c26089d3c90e82ca34
Tamara Dorodnova <tdorodnova@crh.com>
2023-10-10 18:26:36 +0400

    add vw_OriginalConfirmedScheduleLineDeliveryDate

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/intm_s4h/Views/vw_OriginalConfirmedScheduleLineDeliveryDate.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [intm_s4h].[vw_OriginalConfirmedScheduleLineDeliveryDate] AS
+WITH ICDI AS(
+SELECT 
+    SUBSTRING(ChangeDocTableKey,4,10) AS [SalesDocumentItem],
+    SUBSTRING(ChangeDocTableKey,14,6) AS [SalesDocumentItemID],
+    RIGHT(ChangeDocTableKey,4) AS [ScheduleLine],
+    CAST(ChangeDocPreviousFieldValue AS DATE) AS [OriginalConfirmedDeliveryDate]
+FROM
+    [base_s4h_cax].[I_ChangeDocumentItem_VBEP_EDATU]
+)
+SELECT
+   [SalesDocumentItem],
+   [SalesDocumentItemID],
+   [ScheduleLine],
+   MIN([OriginalConfirmedDeliveryDate]) AS [OriginalConfirmedDeliveryDate]
+FROM
+    ICDI
+GROUP BY
+   [SalesDocumentItem],
+   [SalesDocumentItemID],
+   [ScheduleLine]
