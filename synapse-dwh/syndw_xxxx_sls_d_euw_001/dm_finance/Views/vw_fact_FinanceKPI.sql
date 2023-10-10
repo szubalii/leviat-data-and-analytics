80020739c7de95e50d3f3eb0b98af752607edd5e
Michiel Pors <you@example.com>
2023-10-10 17:46:31 +0200

    adding measures

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
@@ -33,3 +33,44 @@ SELECT
 FROM
   [edw].[vw_fact_OpenInvoiced]
 
+UNION
+
+SELECT
+  CompanyCodeID,
+  FiscalYear,
+  FiscalPeriod,
+  FiscalYearPeriod,
+  KPIName
+  KPIValue
+FROM
+  [edw].[vw_fact_VendorInvoice_ApprovedAndPosted_agg]
+
+UNION
+
+SELECT
+  CASE
+    WHEN
+      DATEDIFF(MONTH, ReportDate, OldestOpenItemPostingDate) > 2
+    THEN
+      COUNT(DISTINCT PurchasingDocument)
+  END AS AgedGRPurchaseOrderCount,
+  CASE
+    WHEN
+      DATEDIFF(MONTH, ReportDate, OldestOpenItemPostingDate) > 2
+    THEN
+      SUM(BalAmtInCompanyCodeCry)
+  END AS AmountAgedGRPurchaseOrders
+FROM
+  [edw].[fact_GRIRAccountReconciliation]
+
+UNION
+
+SELECT
+  CASE
+    WHEN
+      DATEDIFF(MONTH, ReportDate, ActualGoodsMovementDate) > 2
+    THEN
+      SUM(OpenInvoicedValue)
+  END AS SONotBilledAmount
+FROM
+  [edw].[vw_fact_ScheduleLineShippedNotBilled]
