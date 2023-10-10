72173166952b1e6d90448d5e4dfd4a00ab2291f6
Michiel Pors <you@example.com>
2023-10-10 17:33:10 +0200

    vim_agg

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_agg.sql
@@ -0,0 +1,19 @@
+CREATE VIEW [edw].[vw_fact_VendorInvoice_ApprovedAndPosted_agg] AS
+SELECT
+  CompanyCodeID,
+  HDR1_FiscalYear,
+  COUNT(DISTINCT AccountingDocument) AS InvoicesCount,
+  CASE
+    WHEN DocumentType IN ('PO_S4, ZPO_S4')
+    THEN COUNT(DISTINCT AccountingDocument)
+  END AS POInvoicesCount,
+  CASE
+    WHEN DocumentType IN ('NPO_S4, ZNPO_S4')
+    THEN COUNT(DISTINCT AccountingDocument)
+  END AS NPOInvoicesCount,
+  CASE
+    WHEN HDR1_NotFirstPass IS NULL
+    THEN COUNT(DISTINCT AccountingDocument)
+  END AS FirstTimePassCount
+FROM
+  [edw].[fact_VendorInvoice_ApprovedAndPosted]
