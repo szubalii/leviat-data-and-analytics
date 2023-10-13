183b9a159847d1bb43944d0de588b0662846d78d
Michiel Pors <you@example.com>
2023-10-13 15:09:47 +0200

    vw_fact_VendorInvoice_ApprovedAndPosted_agg field name change

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_agg.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_agg.sql
@@ -2,18 +2,18 @@ CREATE VIEW [edw].[vw_fact_VendorInvoice_ApprovedAndPosted_agg] AS
 SELECT
   CompanyCodeID,
   HDR1_FiscalYear,
-  COUNT(DISTINCT AccountingDocument) AS InvoicesCount,
+  COUNT(DISTINCT HDR2_AccountingDocument) AS InvoicesCount,
   CASE
     WHEN DocumentType IN ('PO_S4, ZPO_S4')
-    THEN COUNT(DISTINCT AccountingDocument)
+    THEN COUNT(DISTINCT HDR2_AccountingDocument)
   END AS POInvoicesCount,
   CASE
     WHEN DocumentType IN ('NPO_S4, ZNPO_S4')
-    THEN COUNT(DISTINCT AccountingDocument)
+    THEN COUNT(DISTINCT HDR2_AccountingDocument)
   END AS NPOInvoicesCount,
   CASE
     WHEN HDR1_NotFirstPass IS NULL
-    THEN COUNT(DISTINCT AccountingDocument)
+    THEN COUNT(DISTINCT HDR2_AccountingDocument)
   END AS FirstTimePassCount
 FROM
   [edw].[fact_VendorInvoice_ApprovedAndPosted]
