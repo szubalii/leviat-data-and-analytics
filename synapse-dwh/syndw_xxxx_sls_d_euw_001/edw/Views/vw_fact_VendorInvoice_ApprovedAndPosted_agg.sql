5cdd61b095425ed0766098518263ed8e953623ba
Michiel Pors <you@example.com>
2023-10-13 15:12:38 +0200

    update field reference

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_agg.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_VendorInvoice_ApprovedAndPosted_agg.sql
@@ -4,11 +4,11 @@ SELECT
   HDR1_FiscalYear,
   COUNT(DISTINCT HDR2_AccountingDocument) AS InvoicesCount,
   CASE
-    WHEN DocumentType IN ('PO_S4, ZPO_S4')
+    WHEN HDR1_AccountingDocumentTypeID IN ('PO_S4, ZPO_S4')
     THEN COUNT(DISTINCT HDR2_AccountingDocument)
   END AS POInvoicesCount,
   CASE
-    WHEN DocumentType IN ('NPO_S4, ZNPO_S4')
+    WHEN HDR1_AccountingDocumentTypeID IN ('NPO_S4, ZNPO_S4')
     THEN COUNT(DISTINCT HDR2_AccountingDocument)
   END AS NPOInvoicesCount,
   CASE
