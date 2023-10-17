e1b8d9eeb34f1c29f77d01869aa1328cb9a815c8
Michiel Pors <you@example.com>
2023-10-17 16:12:39 +0200

    add tech fields for Address

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_Address.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Tables/dim_Address.sql
@@ -7,6 +7,11 @@ CREATE TABLE [edw].[dim_Address](
   StreetName NVARCHAR(60),
   HouseNumber NVARCHAR(10),
   FullAddress NVARCHAR(170),
+  [t_applicationId]        VARCHAR(32),
+  [t_jobId]                VARCHAR(36),
+  [t_jobDtm]               DATETIME,
+  [t_lastActionCd]         VARCHAR(1),
+  [t_jobBy]                NVARCHAR(128),
   CONSTRAINT [PK_dim_Address] PRIMARY KEY NONCLUSTERED ([AddressID]) NOT ENFORCED
 )
 WITH (
