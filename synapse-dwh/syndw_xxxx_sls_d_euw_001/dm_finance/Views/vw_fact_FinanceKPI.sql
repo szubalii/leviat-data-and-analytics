dd402194a8c0e7a6b2c40e947834c8c0e5cc8250
Michiel Pors <you@example.com>
2023-10-17 15:57:59 +0200

    restructure finance agg

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_FinanceKPI.sql
@@ -1,80 +1,68 @@
 CREATE VIEW [dm_finance].[vw_fact_FinanceKPI] AS
-SELECT
-  CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod,
-  ICOutOfBalance--KPIName
-  -- KPIValue
-FROM
-  [edw].[vw_fact_ACDOCA_EPM_Sales_agg]
 
-UNION
+WITH CompanyCodePeriod AS (
+
+)
+
+
 
 SELECT
-  CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod,
-  ManualJournalEntriesCount--KPIName
-  -- KPIValue
+  ccp.CompanyCodeID,
+  ccp.FiscalYear,
+  ccp.FiscalPeriod,
+  ccp.FiscalYearPeriod,
+  fin_agg.ManualJournalEntriesCount,
+  fin_agg.ManualInventoryAdjustmentsCount,
+  fin_agg.IC_Balance_KPI,
+  oi.OpenInvoicedValue,
+  vim.InvoicesCount,
+  vim.POInvoicesCount,
+  vim.NPOInvoicesCount,
+  vim.FirstTimePassCount,
+  gr.AgedGRPurchaseOrderCount,
+  gr.AgedGRPurchaseOrdersAmount,
+  sls.SOShippedNotBilledAmount
 FROM
-  [edw].[vw_fact_ManualJournalEntriesCount]
+  CompanyCodePeriod
 
-UNION
+LEFT JOIN
+  [edw].[vw_fact_ACDOCA_FinanceKPI_agg] fin_agg
+  ON
+    fin_agg.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    fin_agg.FiscalYearPeriod = ccp.FiscalYearPeriod
 
-SELECT
-  CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod,
-  OpenInvoicedValue--KPIName
-  -- KPIValue
-FROM
-  [edw].[vw_fact_OpenInvoiced]
+LEFT JOIN
+  [dm_finance].[vw_fact_ManualJournalEntriesCount] mj
+  ON
+    mj.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    mj.FiscalYearPeriod = ccp.FiscalYearPeriod
 
-UNION
+LEFT JOIN
+  [edw].[vw_fact_OpenInvoiced] oi
+  ON
+    oi.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    oi.FiscalYearPeriod = ccp.FiscalYearPeriod
 
-SELECT
-  CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod,
-  InvoicesCount,
-  POInvoicesCount,
-  NPOInvoicesCount,
-  FirstTimePassCount
-  -- KPIName
-  -- KPIValue
-FROM
-  [edw].[vw_fact_VendorInvoice_ApprovedAndPosted_agg]
+LEFT JOIN
+  [edw].[vw_fact_VendorInvoice_ApprovedAndPosted_agg] vim
+  ON
+    vim.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    vim.FiscalYearPeriod = ccp.FiscalYearPeriod
 
-UNION
+LEFT JOIN
+  [edw].[vw_fact_GRIRAccountReconciliation_agg] gr
+  ON
+    gr.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    gr.FiscalYearPeriod = ccp.FiscalYearPeriod
 
-SELECT
-  CASE
-    WHEN
-      DATEDIFF(MONTH, ReportDate, OldestOpenItemPostingDate) > 2
-    THEN
-      COUNT(DISTINCT PurchasingDocument)
-  END AS AgedGRPurchaseOrderCount,
-  CASE
-    WHEN
-      DATEDIFF(MONTH, ReportDate, OldestOpenItemPostingDate) > 2
-    THEN
-      SUM(BalAmtInCompanyCodeCry)
-  END AS AmountAgedGRPurchaseOrders
-FROM
-  [edw].[fact_GRIRAccountReconciliation]
-
-UNION
-
-SELECT
-  CASE
-    WHEN
-      DATEDIFF(MONTH, ReportDate, ActualGoodsMovementDate) > 2
-    THEN
-      SUM(OpenInvoicedValue)
-  END AS SONotBilledAmount
-FROM
-  [edw].[vw_fact_ScheduleLineShippedNotBilled]
+LEFT JOIN
+  [edw].[vw_fact_ScheduleLineShippedNotBilled_agg] sls
+  ON
+    sls.CompanyCodeID = ccp.CompanyCodeID
+    AND
+    sls.FiscalYearPeriod = ccp.FiscalYearPeriod
