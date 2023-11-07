8c3eda157a5ea0e35f0dbd53e4a79f5bf0be6da2
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-11-07 14:14:38 +0400

    fixed test

--- synapse-dwh/TestDWH/dm_finance/Views/vw_fact_FinanceKPI/test no empty KPI.sql
+++ synapse-dwh/TestDWH/dm_finance/Views/vw_fact_FinanceKPI/test no empty KPI.sql
@@ -5,17 +5,18 @@ BEGIN
   IF OBJECT_ID('actual') IS NOT NULL DROP TABLE actual;
     
   -- Assemble: Fake Table
-  EXEC tSQLt.FakeTable '[edw]', '[dim_FiscalCalendar]';
-  EXEC tSQLt.FakeTable '[edw]', '[dim_CompanyCode]';
-  EXEC tSQLt.FakeTable '[edw]', '[fact_ACDOCA]';
-  EXEC tSQLt.FakeTable '[edw]', '[dim_ExchangeRates]';
-  EXEC tSQLt.FakeTable '[edw]', '[dim_CurrencyType]';
-  EXEC tSQLt.FakeTable '[edw]', '[dim_ZE_EXQLMAP_DT]';
-  EXEC tSQLt.FakeTable '[edw]', '[fact_VendorInvoice_ApprovedAndPosted]';
-  EXEC tSQLt.FakeTable '[edw]', '[fact_GRIRAccountReconciliation]';
-  EXEC tSQLt.FakeTable '[edw]', '[fact_ScheduleLineShippedNotBilled]';
+  IF OBJECT_ID('tempdb..#dim_FiscalCalendar')                   IS NOT NULL DROP TABLE #dim_FiscalCalendar;
+  IF OBJECT_ID('tempdb..#dim_CompanyCode')                      IS NOT NULL DROP TABLE #dim_CompanyCode;
+  IF OBJECT_ID('tempdb..#fact_ACDOCA')                          IS NOT NULL DROP TABLE #fact_ACDOCA;
+  IF OBJECT_ID('tempdb..#dim_ExchangeRates')                    IS NOT NULL DROP TABLE #dim_ExchangeRates;
+  IF OBJECT_ID('tempdb..#dim_CurrencyType')                     IS NOT NULL DROP TABLE #dim_CurrencyType;
+  IF OBJECT_ID('tempdb..#dim_ZE_EXQLMAP_DT')                    IS NOT NULL DROP TABLE #dim_ZE_EXQLMAP_DT;
+  IF OBJECT_ID('tempdb..#fact_VendorInvoice_ApprovedAndPosted') IS NOT NULL DROP TABLE #fact_VendorInvoice_ApprovedAndPosted;
+  IF OBJECT_ID('tempdb..#fact_GRIRAccountReconciliation')       IS NOT NULL DROP TABLE #fact_GRIRAccountReconciliation;
+  IF OBJECT_ID('tempdb..#fact_ScheduleLineShippedNotBilled')    IS NOT NULL DROP TABLE #fact_ScheduleLineShippedNotBilled;
+  
    
-  INSERT INTO edw.vw_fact_ACDOCA_EPM_Base (
+  INSERT INTO #dim_FiscalCalendar (
     FiscalYear,
     FiscalPeriod,
     FiscalYearPeriod
@@ -25,13 +26,17 @@ BEGIN
     ('2023', '009', '2023009'),
     ('2023', '010', '2023010');
 
-  INSERT INTO edw.dim_CompanyCode (
+  INSERT INTO edw.dim_FiscalCalendar SELECT * FROM #dim_FiscalCalendar;
+
+  INSERT INTO #dim_CompanyCode (
     CompanyCodeID
   )
   VALUES
     ('NZ35');
 
-  INSERT INTO edw.fact_ACDOCA (
+  INSERT INTO edw.dim_CompanyCode SELECT * FROM #dim_CompanyCode;
+
+  INSERT INTO #fact_ACDOCA (
     [CompanyCodeID],
     [FiscalYear],
     [FiscalPeriod],
@@ -74,7 +79,9 @@ BEGIN
     '12345',
     '1111');
 
-  INSERT INTO edw.dim_ExchangeRates (
+  INSERT INTO edw.fact_ACDOCA SELECT * FROM #fact_ACDOCA;
+
+  INSERT INTO #dim_ExchangeRates (
     SourceCurrency,
     TargetCurrency,
     ExchangeRate,
@@ -85,7 +92,9 @@ BEGIN
   ('GBP','EUR','1.1', '2020-01-01', 'P'),
   ('USD','EUR','1.2', '2020-01-01', 'P');
 
-  INSERT INTO edw.dim_ZE_EXQLMAP_DT (
+  INSERT INTO edw.dim_ExchangeRates SELECT * FROM #dim_ExchangeRates;
+
+  INSERT INTO #dim_ZE_EXQLMAP_DT (
     GLAccountID,
     FunctionalAreaID,
     CONTIGENCY4,
@@ -95,21 +104,22 @@ BEGIN
   ( '1111', 'TST1', 'Gross Margin',null)
   ,( '1112', 'TST1', null, 'Opex');
 
- INSERT INTO edw.dim_CurrencyType (
-  CurrencyTypeID,
-  CurrencyType
- )
- VALUES
- ('00','Local'),
- ('10','Transaction'),
- ('20','Group EUR'),
- ('30','Group USD');
-  -- Act: 
-  SELECT *
-  INTO actual
-  FROM [dm_finance].[vw_fact_ACDOCA_EPMSalesView];
+  INSERT INTO edw.dim_ZE_EXQLMAP_DT  SELECT * FROM #dim_ZE_EXQLMAP_DT;
 
-  INSERT INTO [edw].[fact_VendorInvoice_ApprovedAndPosted] (
+  INSERT INTO #dim_CurrencyType (
+    CurrencyTypeID,
+    CurrencyType
+  )
+  VALUES
+    ('00','Local'),
+    ('10','Transaction'),
+    ('20','Group EUR'),
+    ('30','Group USD');
+
+  INSERT INTO edw.dim_CurrencyType SELECT * FROM #dim_CurrencyType;
+
+
+  INSERT INTO #fact_VendorInvoice_ApprovedAndPosted (
     CompanyCodeID,
     HDR1_PostingDate,
     HDR2_AccountingDocument,
@@ -120,7 +130,9 @@ BEGIN
     ('NZ35', '2023-08-01', 1, 'PO_S4', '')
     ,('NZ35', '2023-08-01', 1, 'NPO_S4', '');
 
-  INSERT INTO [edw].[fact_GRIRAccountReconciliation] (
+  INSERT INTO [edw].[fact_VendorInvoice_ApprovedAndPosted] SELECT * FROM #fact_VendorInvoice_ApprovedAndPosted;
+
+  INSERT INTO #fact_GRIRAccountReconciliation (
     CompanyCodeID,
     ReportDate,
     OldestOpenItemPostingDate,
@@ -132,7 +144,9 @@ BEGIN
     ,('NZ35', '2023-08-01', '2023-09-01',1,100)
     ,('NZ35', '2023-08-01', '2023-04-01',2,100);
 
-  INSERT INTO [edw].[fact_ScheduleLineShippedNotBilled]
+  INSERT INTO [edw].[fact_GRIRAccountReconciliation] SELECT * FROM #fact_GRIRAccountReconciliation;
+
+  INSERT INTO #fact_ScheduleLineShippedNotBilled
   ( CompanyCode,
     ReportDate,
     SDI_ODB_LatestActualGoodsMovmtDate,
@@ -142,6 +156,8 @@ BEGIN
     ,('NZ35', '2023-09-01', '2023-04-01', 100)
     ,('NZ35', '2023-10-01', '2023-04-01', 100)
     ,('NZ35', '2023-11-01', '2023-04-01', 100);
+
+  INSERT INTO [edw].[fact_ScheduleLineShippedNotBilled] SELECT * FROM #fact_ScheduleLineShippedNotBilled;
 
 -- Act: 
   SELECT
