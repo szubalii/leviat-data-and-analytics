346552bb751dc5dce2ff2c94d9a18b863c8bb417
Tamara Dorodnova <tdorodnova@crh.com>
2023-11-09 14:23:56 +0400

    improve view, add comment to function

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_getOTIF_OnTimeInFull.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_getOTIF_OnTimeInFull.sql
@@ -3,6 +3,15 @@
     @OT_IsOnTime BIT,
     @IF_IsInFull BIT
 )
+/*fucntion calculates the combination of Is Shipped in Full, and Is Shipped on time.
+In full meaning that the quantity that was expected to be shipped is shipped,
+and shipped on time means that the confirmed delivery date is the date on which is shipped.
+Combining these two KPI's is a main indicator of Supply Chain Performance.
+If not on time, and not in full = NOTNIF.
+On time, and in full = OTIF.
+Not on time, and not in full = NOTIF.
+On time, and not in full = OTNIF.
+*/ 
 RETURNS NVARCHAR(6)
 AS
 BEGIN
