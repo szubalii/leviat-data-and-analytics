fa1563e6e1a31600c7d51692c04856495e713312
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-11-13 12:49:01 +0400

    Fixed aggregation

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_GRIRAccountReconciliation_agg.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_fact_GRIRAccountReconciliation_agg.sql
@@ -1,42 +1,18 @@
-CREATE VIEW [edw].[vw_fact_GRIRAccountReconciliation_agg] AS
-
-WITH By_OldestOpenItemPostingDate AS (
-  SELECT
-    CompanyCodeID,
-    YEAR(ReportDate)       AS FiscalYear,
-    MONTH(ReportDate)      AS FiscalPeriod,
-    edw.svf_getYearPeriod(ReportDate) AS FiscalYearPeriod,
-    CASE
-      WHEN
-        DATEDIFF(MONTH, OldestOpenItemPostingDate, ReportDate) > 2
-      THEN
-        COUNT(DISTINCT PurchasingDocument)
-    END AS AgedGRPurchaseOrderCount,
-    CASE
-      WHEN
-        DATEDIFF(MONTH, OldestOpenItemPostingDate, ReportDate) > 2
-      THEN
-        SUM(BalAmtInCompanyCodeCrcy)
-    END AS AgedGRPurchaseOrdersAmount
-  FROM
-    [edw].[fact_GRIRAccountReconciliation]
-  GROUP BY
-    CompanyCodeID,
-    ReportDate,
-    OldestOpenItemPostingDate
-)
-
+CREATE VIEW [edw].[vw_fact_GRIRAccountReconciliation_agg]
+AS
 SELECT
   CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod,
-  SUM(AgedGRPurchaseOrderCount) AS AgedGRPurchaseOrderCount,
-  SUM(AgedGRPurchaseOrdersAmount) AS AgedGRPurchaseOrdersAmount
+  YEAR(ReportDate)       AS FiscalYear,
+  MONTH(ReportDate)      AS FiscalPeriod,
+  edw.svf_getYearPeriod(ReportDate) AS FiscalYearPeriod,
+  COUNT(DISTINCT PurchasingDocument)AS AgedGRPurchaseOrderCount,
+  SUM(BalAmtInCompanyCodeCrcy)      AS AgedGRPurchaseOrdersAmount
 FROM
-  By_OldestOpenItemPostingDate
+  [edw].[fact_GRIRAccountReconciliation]
+WHERE
+  DATEDIFF(MONTH, OldestOpenItemPostingDate, ReportDate) > 2
 GROUP BY
   CompanyCodeID,
-  FiscalYear,
-  FiscalPeriod,
-  FiscalYearPeriod
+  YEAR(ReportDate),
+  MONTH(ReportDate),
+  edw.svf_getYearPeriod(ReportDate)
