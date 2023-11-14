e098e04315ba8f8e7ccbc39db979f43daac62a52
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-11-14 18:56:03 +0400

    FIll GLAccountID with zeros

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_POGLAccount.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_POGLAccount.sql
@@ -1,6 +1,12 @@
 CREATE VIEW [edw].[vw_POGLAccount]
 AS
 SELECT
-    [GLAccountID]          
+    RIGHT(
+        CONCAT(
+            '0000000000'
+            ,[GLAccountID]
+        )
+        ,10
+    )                               AS [GLAccountID]
     ,[GLAccountDescription] 
 FROM base_ff.POAccount
