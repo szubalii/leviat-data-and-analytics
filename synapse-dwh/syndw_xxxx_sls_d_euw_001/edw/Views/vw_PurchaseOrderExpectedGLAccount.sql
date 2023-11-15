68781982e9683cf0a08a492c1681ba939dedeaf7
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-11-15 16:26:04 +0400

    Fixes

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_PurchaseOrderExpectedGLAccount.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_PurchaseOrderExpectedGLAccount.sql
@@ -1,8 +1,8 @@
 CREATE VIEW [edw].[vw_PurchaseOrderExpectedGLAccount]
 AS
 SELECT
-    RIGHT(
-        CONCAT(
+    RIGHT(                              -- add leading zeros to GLAccountID
+        CONCAT(                         -- if they weren't added in file reference
             '0000000000'
             ,[GLAccountID]
         )
