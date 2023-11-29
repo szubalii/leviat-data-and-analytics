3ff9d10b61e81343886b0671fdf784e8744c7250
Tamara Dorodnova <tdorodnova@crh.com>
2023-11-29 10:39:43 +0400

    add Reconciliation data

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_ff/Tables/Reconciliation.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [base_ff].[Reconciliation] (
+    [ReportingEntity]               NVARCHAR(40),
+    [CompanyCode]                   NVARCHAR(4),
+    [ExQLReportingEntity]           NVARCHAR(20),
+    [Key]                           NVARCHAR(24),
+    [YTD]                           DATE,
+    [HFMvaluesIN$M]                 DECIMAL(15, 3),
+    [t_applicationId] [varchar](32) NULL,
+	[t_jobId] [varchar](36) NULL,
+	[t_jobDtm] [datetime] NULL,
+	[t_jobBy] [varchar](128) NULL,
+	[t_filePath] [nvarchar](1024) NULL
+
+)
+WITH (HEAP, DISTRIBUTION = REPLICATE);
---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_finance/Views/vw_fact_Reconciliation.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dm_finance].[vw_fact_Reconciliation] AS
+SELECT
+     [ReportingEntity]
+    ,[CompanyCode]
+    ,[ExQLReportingEntity]
+    ,[Key]
+    ,[YTD]
+    ,[HFMvaluesIN$M]
+    ,[t_applicationId]
+	,[t_jobDtm]
+FROM
+    [base_ff].[Reconciliation]
