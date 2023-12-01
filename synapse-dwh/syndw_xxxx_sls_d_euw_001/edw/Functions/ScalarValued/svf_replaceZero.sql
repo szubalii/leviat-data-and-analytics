52517d81199e07649c47f2f1664c44239bd2aac4
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-12-01 12:54:46 +0400

    Fixed function

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_replaceZero.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Functions/ScalarValued/svf_replaceZero.sql
@@ -3,9 +3,12 @@ CREATE FUNCTION [edw].[svf_replaceZero](
     ,@secondValue   decimal
 )
 RETURNS decimal
+-- that function receives 2 args
+-- if first arg not equal zero it returns first arg
+-- otherwise it returns second arg
 AS
 BEGIN
-    IF @firstValue <> 0 THEN
+    IF @firstValue <> 0
         RETURN @firstValue
     ELSE
         RETURN @secondValue;
