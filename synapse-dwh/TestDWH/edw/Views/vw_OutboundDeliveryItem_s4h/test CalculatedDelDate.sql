a40977715377f73851f5bfd646f03b45e596142f
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-12-05 12:39:28 +0400

    Fixed tests

--- synapse-dwh/TestDWH/edw/Views/vw_OutboundDeliveryItem_s4h/test CalculatedDelDate.sql
+++ synapse-dwh/TestDWH/edw/Views/vw_OutboundDeliveryItem_s4h/test CalculatedDelDate.sql
@@ -2,8 +2,8 @@ CREATE PROCEDURE [tc.edw.vw_OutboundDeliveryItem_s4h].[test CalculatedDelDate]
 AS
 BEGIN
 
-  IF OBJECT_ID('actual') IS NOT NULL DROP TABLE actual;
-  IF OBJECT_ID('expected') IS NOT NULL DROP TABLE expected;
+  IF OBJECT_ID('actual')    IS NOT NULL DROP TABLE actual;
+  IF OBJECT_ID('expected')  IS NOT NULL DROP TABLE expected;
 
   -- Assemble: Fake Table
   EXEC tSQLt.FakeTable '[base_s4h_cax]', '[I_OutboundDeliveryItem]';
@@ -26,7 +26,7 @@ BEGIN
   ,(200, 3, '2023-01-01', 50, 2)          -- CalculatedDelDate NULL
   ,(200, 4, '2023-01-01', 60, 1)          -- CalculatedDelDate NULL
   ,(200, 5, '2023-01-01', 70, 1)          -- CalculatedDelDate = ActualGoodsMovementDate + 1 DAY
-  ,(200, 6, '2023-01-01', 70, 3)          -- CalculatedDelDate = ActualGoodsMovementDate
+  ,(200, 6, '2023-01-01', 70, 3);         -- CalculatedDelDate = ActualGoodsMovementDate
 
   INSERT INTO [edw].[dim_Route] ([ROUTEID], [DurInDays], [ROUTE], [TRAZT], [TDVZT], [SPFBK], [EXPVZ], [TDIIX])
   VALUES
@@ -42,7 +42,7 @@ BEGIN
     [HDR_ActualDeliveryRoute],
     [CalculatedDelDate]
   INTO actual
-  FROM [edw].[vw_OutboundDeliveryItem_s4h]
+  FROM [edw].[vw_OutboundDeliveryItem_s4h];
 
   SELECT TOP(0) *
   INTO expected
