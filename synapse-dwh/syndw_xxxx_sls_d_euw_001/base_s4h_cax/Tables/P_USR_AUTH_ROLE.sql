f9f90c0e29b2c7972320f2fea35f28946f8a68cb
Tamara Dorodnova <tdorodnova@crh.com>
2023-12-08 15:43:14 +0400

    fix P_USR_AUTH_ROLE entity

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/P_USR_AUTH_ROLE.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/P_USR_AUTH_ROLE.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [base_s4h_cax].[P_USR_AUTH_ROLE] (    
     [MANDT] CHAR(3) NOT NULL  -- Client
-  , [BNAME] NVARCHAR(12)  -- User Name in User Master Record
-  , [profile] NVARCHAR(12)  -- Profile name
+  , [BNAME] NVARCHAR(12) NOT NULL -- User Name in User Master Record
+  , [profile] NVARCHAR(12) NOT NULL-- Profile name
   , [agr_name] NVARCHAR(30)  -- Role Name
   , [refuser] NVARCHAR(12)  -- Reference user
-  , [from_dat] DATE  -- Date of validity
+  , [from_dat] DATE NOT NULL-- Date of validity
   , [to_dat] DATE  -- Date of validity
   , [col_flag] NVARCHAR(1)  -- Flag: Assignment from composite role
   , [org_flag] NVARCHAR(1)  -- Flag: Assignment Comes From HR Organization Management
