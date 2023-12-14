f89472a412cc8fb8844ecc3f058230042e03c7da
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-12-14 20:37:33 +0400

    Added source tables to auto test

--- synapse-dwh/TestDWH/edw/Views/vw_ACDOCA_ReferenceSalesDocument/test uniqueness.sql
+++ synapse-dwh/TestDWH/edw/Views/vw_ACDOCA_ReferenceSalesDocument/test uniqueness.sql
@@ -7,6 +7,8 @@ BEGIN
 
   -- Assemble: Fake Table
   EXEC tSQLt.FakeTable '[base_s4h_cax]', '[I_GLAccountLineItemRawData_202301]';
+  EXEC tSQLt.FakeTable '[base_s4h_cax]', '[I_SDDocumentProcessFlow]';
+  EXEC tSQLt.FakeTable '[edw]', '[fact_SalesDocumentItem]';
   
   INSERT INTO base_s4h_cax.I_GLAccountLineItemRawData_202301 (
      [SourceLedger]
@@ -15,10 +17,34 @@ BEGIN
     ,[AccountingDocument]
     ,[LedgerGLLineItem]
     ,[AccountingDocumentType]
+    ,[SalesDocumentID]
+    ,[SalesDocumentItemID]
     )
   VALUES 
-    ('TS', 'TS35', 2023, '00123', '000001', 'DC')
-    ,('TS', 'TS35', 2023, '00123', '000001', 'AC');
+    ('TS', 'TS35', 2023, '00123', '000001', 'DC', '1', '000001')
+    ,('TS', 'TS35', 2023, '00123', '000001', 'AC', '2', '000001');
+
+  INSERT INTO base_s4h_cax.I_SDDocumentProcessFlow (
+     [PrecedingDocument]
+    ,[PrecedingDocumentItem]
+    ,[SubsequentDocumentCategory]
+    ,[PrecedingDocumentCategory]
+    ,[SubsequentDocument]
+    ,[SubsequentDocumentItem]
+  )
+  VALUES
+  ('1', '000001', 'C', 'D', '11', '000010')
+  ,('2', '000001', 'C', 'D', '12', '000010')
+  ,('10', '000001', 'C', 'C', '1', '000001')
+  ,('20', '000001', 'C', 'C', '2', '000001');
+
+  INSERT INTO edw.fact_SalesDocumentItem (
+    SalesDocument
+    ,SalesDocumentItem
+  )
+  VALUES
+   ('1', '000001')
+  ,('2', '000001');
 
   -- Act: 
   SELECT
