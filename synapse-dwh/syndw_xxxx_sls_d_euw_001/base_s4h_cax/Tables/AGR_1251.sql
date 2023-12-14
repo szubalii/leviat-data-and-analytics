36c59909184e3087885b91ebceeef771bea8cd83
Tamara Dorodnova <tdorodnova@crh.com>
2023-12-14 16:08:04 +0400

    add AGR_1251, AGR_TEXTS, TSTCT entities

---
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/AGR_1251.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [base_s4h_cax].[AGR_1251] (
+    [MANDT] CHAR(3) NOT NULL
+  , [Agr_Name] NVARCHAR(30) NOT NULL
+  , [Counter] NVARCHAR(6) NOT NULL
+  , [Object] NVARCHAR(10)
+  , [Auth] NVARCHAR(12)
+  , [Variant] NVARCHAR(4)
+  , [Field] NVARCHAR(10)
+  , [Low] NVARCHAR(40)
+  , [High] NVARCHAR(40)
+  , [Modified] CHAR(1)
+  , [Deleted] CHAR(1)
+  , [Copied] CHAR(1)
+  , [Neu] CHAR(1)
+  , [Node] NVARCHAR(3)
+  , [t_applicationId] VARCHAR (32)
+  , [t_jobId] VARCHAR (36)
+  , [t_jobDtm] DATETIME
+  , [t_jobBy] VARCHAR (128)
+  , [t_extractionDtm] DATETIME
+  , [t_filePath] NVARCHAR (1024)
+  , CONSTRAINT [PK_AGR_1251] PRIMARY KEY NONCLUSTERED(
+      [MANDT],[Agr_Name],[Counter]
+  ) NOT ENFORCED
+) WITH (
+  HEAP
+)
