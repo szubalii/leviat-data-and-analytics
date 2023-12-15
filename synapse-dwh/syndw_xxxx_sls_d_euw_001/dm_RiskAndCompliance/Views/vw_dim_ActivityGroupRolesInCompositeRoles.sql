0c4bc750289d8a6835595dec5ea6d6675f0fa492
Tamara Dorodnova <tdorodnova@crh.com>
2023-12-15 14:41:30 +0400

    fix remarks from PR

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_RiskAndCompliance/Views/vw_dim_ActivityGroupRolesInCompositeRoles.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_RiskAndCompliance/Views/vw_dim_ActivityGroupRolesInCompositeRoles.sql
@@ -1,10 +1,24 @@
 ﻿CREATE VIEW [dm_RiskAndCompliance].[vw_dim_ActivityGroupRolesInCompositeRoles]
 AS
+WITH AGR_TEXTS_SINGLE_LINE AS (
+  SELECT
+    [Agr_Name],
+    STRING_AGG([Text], ' ') WITHIN GROUP ( ORDER BY [Line] ASC ) AS [Text]
+  FROM
+    [base_s4h_cax].[AGR_TEXTS]
+  GROUP BY
+    [Agr_Name]
+)
 SELECT
-     [Agr_Name]
-    ,[Child_Agr]
-    ,[Attributes]
-    ,[t_applicationId]
-    ,[t_extractionDtm]
+     AA.[Agr_Name] AS [CompositeActivityGroupRoleName]
+    ,AA.[Child_Agr] AS [ActivityGroupRoleName]
+    ,AA.[Attributes]
+    ,AGR_TEXTS_SINGLE_LINE.[Text]
+    ,AA.[t_applicationId]
+    ,AA.[t_extractionDtm]
 FROM
-    [base_s4h_cax].[AGR_AGRS]
+    [base_s4h_cax].[AGR_AGRS] AA
+LEFT JOIN
+    AGR_TEXTS_SINGLE_LINE
+    ON
+        AA.[Child_Agr] = AGR_TEXTS_SINGLE_LINE.[Agr_Name]
