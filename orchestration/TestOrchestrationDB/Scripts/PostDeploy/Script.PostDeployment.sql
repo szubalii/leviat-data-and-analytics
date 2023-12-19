f672e8f72f5962d281e5f2e76c19f44a35d287f2
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-12-19 11:18:31 +0400

    Fix run

--- orchestration/TestOrchestrationDB/Scripts/PostDeploy/Script.PostDeployment.sql
+++ orchestration/TestOrchestrationDB/Scripts/PostDeploy/Script.PostDeployment.sql
@@ -1,10 +1,20 @@
 -- This file contains SQL statements that will be executed after the build script.
 
-:r ..\..\TestClasses\DirectoryPath.sql
-:r ..\..\TestClasses\EntityFile.sql
-:r ..\..\TestClasses\LoadTypeEntities.sql
-:r ..\..\TestClasses\ScheduledEntities.sql
-:r ..\..\TestClasses\ScalarValuedFunction.sql
-:r ..\..\TestClasses\StoredProcedures.sql
-:r ..\..\TestClasses\ForeignKeys.sql
+/*
+Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.		
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
 -- :r ..\..\TestClasses\FailTest.sql
+
+:r .\Script.AddSchemasAsTestClasses.sql
+
+EXEC [tSQLt].[SetFakeViewOff] 'edw';
+GO
