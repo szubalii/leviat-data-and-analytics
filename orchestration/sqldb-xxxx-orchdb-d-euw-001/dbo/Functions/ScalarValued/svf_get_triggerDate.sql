b5ca5435db91caf5ee8b4992f8bfe537cd753fa3
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2023-12-28 16:01:52 +0400

    added support for wildcard

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/ScalarValued/svf_get_triggerDate.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/ScalarValued/svf_get_triggerDate.sql
@@ -8,7 +8,9 @@ CREATE FUNCTION [dbo].[svf_get_triggerDate](
 RETURNS DATE
 AS
 BEGIN
-  DECLARE @date_string AS CHAR(23) = LEFT(RIGHT(@file_name, 31), 23);
+  DECLARE @i AS INT = PATINDEX('%[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9]%', @file_name)
+  -- added pattern match to support filenames with star
+  DECLARE @date_string AS CHAR(23) = SUBSTRING(@file_name, @i, 23);
   DECLARE @trigger_date AS DATE = DATEFROMPARTS(
     LEFT(@date_string, 4),
     SUBSTRING(@date_string, 6, 2),
