27d21b5dae1e168cce82c5b19d727d4b77ce4fcc
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2024-01-03 18:20:07 +0400

    Added test

---
+++ synapse-dwh/TestDWH/edw/Views/vw_OutboundDeliveryItem_s4h/test SL_OriginalConfirmedDeliveryDate.sql
@@ -0,0 +1,61 @@
+CREATE PROCEDURE [tc.edw.vw_OutboundDeliveryItem_s4h].[test SL_OriginalConfirmedDeliveryDate]
+AS
+BEGIN
+
+  IF OBJECT_ID('actual') IS NOT NULL DROP TABLE actual;
+  IF OBJECT_ID('expected') IS NOT NULL DROP TABLE expected;
+
+  -- Assemble: Fake Table
+  EXEC tSQLt.FakeTable '[base_s4h_cax]', '[I_OutboundDeliveryItem]';
+  EXEC tSQLt.FakeTable '[edw]', '[vw_fact_SalesDocumentItem_LC_EUR]';
+  EXEC tSQLt.FakeTable '[intm_s4h]', '[vw_SalesDocumentEarliestConfirmedDeliveryDate]';
+  EXEC tSQLt.FakeTable '[intm_s4h]', '[vw_OriginalConfirmedScheduleLineDeliveryDate]';
+
+
+  INSERT INTO [base_s4h_cax].[I_OutboundDeliveryItem] ([ReferenceSDDocument], [ReferenceSDDocumentItem], [ActualDeliveryQuantity])
+  VALUES (1, 1, 100), (2, 2, 200);
+
+  INSERT INTO [edw].[vw_fact_SalesDocumentItem_LC_EUR] ([SalesDocument], [SalesDocumentItem], [SDI_ConfdDelivQtyInOrderQtyUnit])
+  VALUES
+    (1, 1, 110),
+    (2, 2, 200);
+  
+  INSERT INTO [intm_s4h].[vw_SalesDocumentEarliestConfirmedDeliveryDate] ([SalesDocument], [SalesDocumentItem], [ConfirmedDeliveryDate], [ScheduleLine])
+  VALUES
+    (1, 1, '2024-01-01', 1),
+    (1, 1, '2023-12-01', 2),
+    (2, 2, '2023-01-01', 3),
+    (2, 2, '2023-06-01', 4);
+  
+  INSERT INTO [intm_s4h].[vw_OriginalConfirmedScheduleLineDeliveryDate] ([SalesDocumentID], [SalesDocumentItemID], [OriginalConfirmedDeliveryDate], [ScheduleLine])
+  VALUES
+    (1, 1, '2024-01-01', 1),
+    (1, 1, '2023-11-01', 2),
+    (2, 2, '2023-02-01', 3),
+    (2, 2, '2023-07-01', 4);
+
+  -- Act: 
+  SELECT
+    [ReferenceSDDocument],
+    [ReferenceSDDocumentItem],
+    [SL_OriginalConfirmedDeliveryDate]
+  INTO actual
+  FROM [edw].[vw_OutboundDeliveryItem_s4h]
+
+  SELECT TOP(0) *
+  INTO expected
+  FROM actual;
+
+  INSERT INTO expected(
+    [ReferenceSDDocument],
+    [ReferenceSDDocumentItem],
+    [SL_FirstCustomerRequestedDeliveryDate])
+  VALUES
+    (1, 1,'2023-11-01')
+    ,(1, 1,'2023-11-01')
+    ,(2, 2,'2023-01-01')
+    ,(2, 2,'2023-06-01');
+
+  -- Assert:
+  EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+END;
