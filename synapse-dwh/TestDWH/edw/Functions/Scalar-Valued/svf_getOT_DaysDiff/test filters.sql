afd83d91fb8a3a177e530fc0092e0adbb3c79050
Michiel Pors <adm_mpors@crhemea.onmicrosoft.com>
2024-01-09 08:41:26 +0100

    reformat [tc.edw.svf_getOT_DaysDiff].[test filters]

--- synapse-dwh/TestDWH/edw/Functions/Scalar-Valued/svf_getOT_DaysDiff/test filters.sql
+++ synapse-dwh/TestDWH/edw/Functions/Scalar-Valued/svf_getOT_DaysDiff/test filters.sql
@@ -12,21 +12,21 @@ BEGIN
     Cur_date DATE
   );
 
-  INSERT INTO testdata (DeliveryDate,CalculatedDate,Cur_date)
+  INSERT INTO testdata (DeliveryDate, CalculatedDate, Cur_date)
   VALUES 
-    (NULL,'2023-10-18','2023-10-24'),
-    ('0001-01-01','2023-10-18','2023-10-24'),
-    ('2023-10-18',NULL,'2023-10-24'),
-    ('2023-10-18','0001-01-01','2023-10-24'),
-    ('2023-10-18','2023-10-23','2023-10-24'),
-    ('2023-10-15','2023-10-21','2023-10-24');
+    (        NULL, '2023-10-18', '2023-10-24'),
+    ('0001-01-01', '2023-10-18', '2023-10-24'),
+    ('2023-10-18',         NULL, '2023-10-24'),
+    ('2023-10-18', '0001-01-01', '2023-10-24'),
+    ('2023-10-18', '2023-10-23', '2023-10-24'),
+    ('2023-10-15', '2023-10-21', '2023-10-24');
 
   ---- Act:
   SELECT
     DeliveryDate,
     CalculatedDate,
     Cur_date,
-    [edw].[svf_getOT_DaysDiff](DeliveryDate,CalculatedDate,Cur_date) AS OT_DaysDiff
+    [edw].[svf_getOT_DaysDiff](DeliveryDate, CalculatedDate,Cur_date) AS OT_DaysDiff
   INTO actual
   FROM testdata;
 
@@ -38,14 +38,14 @@ BEGIN
     OT_DaysDiff INT
   );
 
-  INSERT INTO expected (DeliveryDate,CalculatedDate,Cur_date,OT_DaysDiff)
+  INSERT INTO expected (DeliveryDate, CalculatedDate, Cur_date, OT_DaysDiff)
   VALUES 
-    (NULL,'2023-10-18','2023-10-24', NULL),
-    ('0001-01-01','2023-10-18','2023-10-24', NULL),
-    ('2023-10-18',NULL,'2023-10-24', NULL),
-    ('2023-10-18','0001-01-01','2023-10-24', NULL),
-    ('2023-10-18','2023-10-23','2023-10-24', 3),
-    ('2023-10-15','2023-10-21','2023-10-24', 6);
+    (        NULL, '2023-10-18', '2023-10-24', NULL),
+    ('0001-01-01', '2023-10-18', '2023-10-24', NULL),
+    ('2023-10-18',         NULL, '2023-10-24', NULL),
+    ('2023-10-18', '0001-01-01', '2023-10-24', NULL),
+    ('2023-10-18', '2023-10-23', '2023-10-24',    3),
+    ('2023-10-15', '2023-10-21', '2023-10-24',    6);
 
   EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
 END;
