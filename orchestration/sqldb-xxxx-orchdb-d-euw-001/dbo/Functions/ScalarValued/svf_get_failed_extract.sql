9201677812ad62405619805eb57a2f6525c86979
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2024-01-24 14:44:21 +0100

    fixed svf

--- orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/ScalarValued/svf_get_failed_extract.sql
+++ orchestration/sqldb-xxxx-orchdb-d-euw-001/dbo/Functions/ScalarValued/svf_get_failed_extract.sql
@@ -7,17 +7,17 @@ CREATE FUNCTION [dbo].[svf_get_failed_extract](
 RETURNS TINYINT
 AS
 BEGIN
-  DECLARE @failed_extract AS INTEGER;
+  DECLARE @failed_extract TINYINT;
 
   SELECT 
-    MIN(
+    @failed_extract = MIN(
       CASE 
         WHEN status_id = 2
           THEN 0
         ELSE
           1
       END
-     ) INTO @failed_extract
+     )
     FROM
       dbo.batch
   WHERE
