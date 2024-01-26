27ff76759747965bde55d80504b1d1e5441cc990
Nikolai Kukhtenko <Nikolai_Kukhtenko@epam.com>
2024-01-26 14:41:53 +0100

    Fixed test

--- orchestration/TestOrchestrationDB/dbo/Functions/ScalarValued/svf_get_isRequired_delta_batch_activity/test file after failure.sql
+++ orchestration/TestOrchestrationDB/dbo/Functions/ScalarValued/svf_get_isRequired_delta_batch_activity/test file after failure.sql
@@ -3,7 +3,7 @@ AS
 BEGIN  
   -- Act: 
   DECLARE @actual INT = ( SELECT dbo.svf_get_isRequired_delta_batch_activity(
-    'test2', 'test1', 200, 300, 0
+    'test2', 'test1', 200, 300
   ));
 
   -- Assert:
