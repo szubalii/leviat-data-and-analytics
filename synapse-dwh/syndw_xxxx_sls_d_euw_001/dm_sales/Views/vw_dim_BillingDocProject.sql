32ccd30a83061e34b1309d567b8a23fba8709a13
Solomiya Leshchuk <sleshchuk@crh.com>
2024-01-29 17:16:02 +0200

    add fix for getting maxProject

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_BillingDocProject.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/dm_sales/Views/vw_dim_BillingDocProject.sql
@@ -1,20 +1,31 @@
 CREATE VIEW [dm_sales].[vw_dim_BillingDocProject]
 AS
+WITH maxProject AS (
+  SELECT
+        ProjectID
+      , MAX([Project]) OVER (PARTITION BY [ProjectID]) AS Project
+      , MAX([ProjectID_Name]) OVER (PARTITION BY [ProjectID]) AS ProjectID_Name
+      , [t_applicationId]
+      , [t_extractionDtm]
+  FROM 
+    edw.[dim_BillingDocProject]
+
+)
 -- get list of unique projects
 SELECT
-  ProjectID
-, Project
-, ProjectID_Name
-, NULL AS CityName      -- Power BI expects this field but is not actually used
-, NULL AS StreetName    -- Power BI expects this field but is not actually used
-, NULL AS PostalCode    -- Power BI expects this field but is not actually used
-, [t_applicationId]
-, [t_extractionDtm]
+    ProjectID
+  , Project
+  , ProjectID_Name
+  , NULL AS CityName      -- Power BI expects this field but is not actually used
+  , NULL AS StreetName    -- Power BI expects this field but is not actually used
+  , NULL AS PostalCode    -- Power BI expects this field but is not actually used
+  , [t_applicationId]
+  , [t_extractionDtm]
 FROM
-  [edw].[dim_BillingDocProject]
+  maxProject
 GROUP BY
-  ProjectID
-, Project
-, ProjectID_Name
-, [t_applicationId]
-, [t_extractionDtm]
+    ProjectID
+  , Project
+  , ProjectID_Name
+  , [t_applicationId]
+  , [t_extractionDtm]
