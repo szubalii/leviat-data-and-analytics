130995f65f84ec27f17eb796f35c5b26496842ca
Solomiya Leshchuk <sleshchuk@crh.com>
2024-03-06 16:54:35 +0200

    update test for CustomerID

--- synapse-dwh/TestDWH/edw/Views/vw_fact_ACDOCA_EPM_Base/test CustomerID.sql
+++ synapse-dwh/TestDWH/edw/Views/vw_fact_ACDOCA_EPM_Base/test CustomerID.sql
@@ -7,7 +7,6 @@ BEGIN
 
   -- Assemble: Fake Table
   EXEC tSQLt.FakeTable '[edw]', '[fact_ACDOCA]';
-  EXEC tSQLt.FakeTable '[edw]', '[fact_SalesDocumentItem]';
   EXEC tSQLt.FakeTable '[edw]', '[vw_CurrencyConversionRate]';
   EXEC tSQLt.FakeTable '[dm_sales]', '[vw_dim_CurrencyType]';
 
@@ -17,28 +16,17 @@ BEGIN
     CompanyCodeCurrency,
     BillingDocumentTypeID,
     CustomerID,
+    SDI_SoldToPartyID,
     GLAccountID
   )
   VALUES
-  ('SD1', '01', 'EUR', '', '', '0001'),
-  ('SD2', '02', 'EUR', '', NULL, '0002'),
-  ('SD3', '03', 'EUR', 'BT3', '', '0003'),
-  ('SD4', '04', 'EUR', 'XC4', '114', '0004'),
-  ('SD5', '05', 'EUR', 'VM5', '', '0005');
+  ('SD1', '01', 'EUR', '', '', '', '0001'),
+  ('SD2', '02', 'EUR', '', NULL, '', '0002'),
+  ('SD3', '03', 'EUR', 'BT3', '', '333', '0003'),
+  ('SD4', '04', 'EUR', 'XC4', '114','444', '0004'),
+  ('SD5', '05', 'EUR', '', '', '555', '0005');
 
-  INSERT INTO edw.fact_SalesDocumentItem (
-    SalesDocument,
-    SalesDocumentItem,
-    SoldToPartyID,
-    CurrencyTypeID
-  )
-  VALUES
-  ('SD1', '01', '', '10'),
-  ('SD2', '02', '', '10'),
-  ('SD3', '03', '0003', '10'),
-  ('SD4', '04', '', '10'),
-  ('SD5', '05', '', '10');
-
+ 
 
   INSERT INTO dm_sales.vw_dim_CurrencyType (
     CurrencyTypeID
@@ -80,9 +68,9 @@ BEGIN
   VALUES
     ('SD1', '(MA)-0001'),
     ('SD2', '(MA)-0002'),
-    ('SD3', '0003'),
+    ('SD3', ''),
     ('SD4', '114'),
-    ('SD5', '');
+    ('SD5', '555');
 
   EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
 
