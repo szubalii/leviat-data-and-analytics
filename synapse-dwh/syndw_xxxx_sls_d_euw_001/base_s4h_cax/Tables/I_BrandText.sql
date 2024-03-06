441936bceaba8ea4bd390e09165bde39fefd121b
Michiel Pors <adm_mpors@crhemea.onmicrosoft.com>
2024-03-06 09:51:01 +0100

    test system fields

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_BrandText.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/base_s4h_cax/Tables/I_BrandText.sql
@@ -3,12 +3,6 @@ CREATE TABLE [base_s4h_cax].[I_BrandText](
 , [Brand] nvarchar(4) NOT NULL
 , [Language] char(1) collate  Latin1_General_100_BIN2 NOT NULL
 , [BrandName] nvarchar(30)
-, [t_applicationId]       VARCHAR (32)
-, [t_jobId]               VARCHAR (36)
-, [t_jobDtm]              DATETIME
-, [t_jobBy]        		  NVARCHAR (128)
-, [t_extractionDtm]		  DATETIME
-, [t_filePath]            NVARCHAR (1024)
 , CONSTRAINT [PK_I_BrandText] PRIMARY KEY NONCLUSTERED (
     [MANDT], [Brand], [Language]
   ) NOT ENFORCED
@@ -16,3 +10,13 @@ CREATE TABLE [base_s4h_cax].[I_BrandText](
 WITH (
   HEAP
 )
+
+ALTER TABLE
+  [base_s4h_cax].[I_BrandText]
+ADD
+  [t_applicationId] VARCHAR   (32)
+, [t_jobId]         VARCHAR   (36)
+, [t_jobDtm]        DATETIME
+, [t_jobBy]  		   NVARCHAR  (128)
+, [t_extractionDtm]	DATETIME
+, [t_filePath]     NVARCHAR (1024)
