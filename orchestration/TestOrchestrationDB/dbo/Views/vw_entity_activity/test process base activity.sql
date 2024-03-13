f94a3d350f5df69e2fa1cebd6ec714121d41b7f0
Michiel Pors <adm_mpors@crhemea.onmicrosoft.com>
2024-03-13 15:30:24 +0100

    test case for processbase activity

---
+++ orchestration/TestOrchestrationDB/dbo/Views/vw_entity_activity/test process base activity.sql
@@ -0,0 +1,40 @@
+CREATE PROCEDURE [tc.dbo.vw_entity_activity].[test process base activity]
+AS
+BEGIN
+
+  IF OBJECT_ID('actual') IS NOT NULL DROP TABLE actual;
+  IF OBJECT_ID('expected') IS NOT NULL DROP TABLE expected;
+
+  -- Assemble: Fake Table
+  EXEC tSQLt.FakeTable '[dbo]', '[entity]';
+
+  INSERT INTO dbo.entity (entity_id, layer_id, base_sproc_name)
+  VALUES
+    (1, 6, NULL),
+    (2, 6, 'up_upsert_delta_active_table');
+
+  -- Act: 
+  SELECT entity_id, expected_activity_id
+  INTO actual
+  FROM vw_entity_activity;
+
+  -- Assert:
+  SELECT TOP(0) *
+  INTO expected
+  FROM actual;
+
+  INSERT INTO expected(entity_id, expected_activity_id)
+  VALUES
+    (1,21),
+    (1,13),
+    (1,19),
+    (1,2),
+    (1,15),
+    (1,21),
+    (1,13),
+    (1,19),
+    (1,2);
+
+  EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+END;
+GO
