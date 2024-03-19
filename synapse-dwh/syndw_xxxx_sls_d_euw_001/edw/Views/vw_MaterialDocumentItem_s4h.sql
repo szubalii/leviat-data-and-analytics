a68b73556957ec6d4890f6e7677c804a0d086220
Fred Bouw <fbouw@crh.com>
2024-03-19 09:21:19 +0100

    Composite view

--- synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_MaterialDocumentItem_s4h.sql
+++ synapse-dwh/syndw_xxxx_sls_d_euw_001/edw/Views/vw_MaterialDocumentItem_s4h.sql
@@ -137,7 +137,7 @@ SELECT
 , MDI.[MaterialDocumentRecordType]
 , MDI.[t_applicationId] collate DATABASE_DEFAULT AS t_applicationId
 , MDI.[t_extractionDtm]
-FROM [base_s4h_cax].[Z_I_MaterialDocumentItem] MDI
+FROM [edw].[vw_MaterialDocumentItemMerged] MDI
 LEFT JOIN [base_s4h_cax].[I_MaterialDocumentHeader] MDH
   ON 
   MDI.[MaterialDocumentYear]=MDH.[MaterialDocumentYear]
