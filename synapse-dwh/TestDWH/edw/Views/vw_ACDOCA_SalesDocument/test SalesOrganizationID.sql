efde1a23f2f3bea8dc6e8ad5d87f7119272449bc
Solomiya Leshchuk <sleshchuk@crh.com>
2024-04-16 11:28:01 +0300

    test case for vw_ACDOCA_SalesDocument

---
+++ synapse-dwh/TestDWH/edw/Views/vw_ACDOCA_SalesDocument/test SalesOrganizationID.sql
@@ -0,0 +1,82 @@
+CREATE PROCEDURE [tc.edw.vw_ACDOCA_SalesDocument].[test SalesOrganizationID]
+AS
+BEGIN
+
+  IF OBJECT_ID('actual') IS NOT NULL DROP TABLE actual;
+  IF OBJECT_ID('tempdb..#vw_GLAccountLineItemRawData') IS NOT NULL DROP TABLE #vw_GLAccountLineItemRawData;
+  -- IF OBJECT_ID('expected') IS NOT NULL DROP TABLE expected;
+
+  -- Assemble: Fake Table
+  EXEC tSQLt.FakeTable '[edw]', '[vw_GLAccountLineItemRawData]';
+  EXEC tSQLt.FakeTable '[edw]', '[fact_SalesDocumentItem]';
+  
+
+  SELECT TOP(0) *
+  INTO #vw_GLAccountLineItemRawData
+  FROM edw.vw_GLAccountLineItemRawData;
+
+ INSERT INTO #vw_GLAccountLineItemRawData (
+    SourceLedgerID,
+    CompanyCodeID,
+    FiscalYear,
+    AccountingDocument,
+    LedgerGLLineItem,
+    SalesDocumentID
+  )
+  VALUES
+  ('OC', 'DE35', '2024', '9900000007', '000001', '0020071703'),
+  ('OC', 'DE35', '2024', '9900000007', '000002', '0020071703'),
+  ('OC', 'DE35', '2024', '9900000007', '000003', '0020071703'),
+  ('OC', 'DE35', '2024', '9900000007', '000004', ''),
+  ('L1', 'CH35', '2024', '3333333333', '000001', '0020077777');
+  
+
+  EXEC ('INSERT INTO edw.vw_GLAccountLineItemRawData SELECT * FROM #vw_GLAccountLineItemRawData');
+
+
+
+  INSERT INTO edw.fact_SalesDocumentItem (
+     SalesDocument
+    ,SalesOrganizationID
+  )
+  VALUES
+   ('0020071703', 'DE01')
+  ,('0020077777', 'CH01');
+  
+
+  -- Act: 
+  SELECT
+    SourceLedgerID,
+    CompanyCodeID, 
+    FiscalYear,
+    AccountingDocument, 
+    LedgerGLLineItem,
+    SalesOrganizationID_SDI
+  INTO actual
+  FROM [edw].[vw_ACDOCA_SalesDocument];
+
+  -- Assert:
+  SELECT TOP(0) *
+  INTO expected
+  FROM actual;
+
+  INSERT INTO expected (
+    SourceLedgerID,
+    CompanyCodeID, 
+    FiscalYear,
+    AccountingDocument, 
+    LedgerGLLineItem,
+    SalesOrganizationID_SDI
+  )
+  VALUES
+  ('OC', 'DE35', '2024', '9900000007', '000001', 'DE01'),
+  ('OC', 'DE35', '2024', '9900000007', '000002', 'DE01'),
+  ('OC', 'DE35', '2024', '9900000007', '000003', 'DE01'),
+  ('OC', 'DE35', '2024', '9900000007', '000004', NULL),
+  ('L1', 'CH35', '2024', '3333333333', '000001', 'CH01');
+
+
+
+  EXEC tSQLt.AssertEqualsTable 'expected', 'actual';
+
+END
