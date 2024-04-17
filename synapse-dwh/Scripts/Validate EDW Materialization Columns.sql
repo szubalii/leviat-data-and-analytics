8ef88ae57ddf8a9991c66850c3037f5f6ecdf04c
Michiel Pors <you@example.com>
2024-04-17 10:46:00 +0200

    validate edw materialization columns

---
+++ synapse-dwh/Scripts/Validate EDW Materialization Columns.sql
@@ -0,0 +1,98 @@
+DECLARE @entity NVARCHAR(MAX);
+SELECT @entity = BulkColumn
+FROM OPENROWSET(BULK '/global/entity.json', SINGLE_CLOB) as j;
+
+WITH entity AS (
+  SELECT
+    -- [key]+1 AS [column_id], -- array index starts at 0, so add 1
+    -- JSON_VALUE(value, '$.edw.entities') AS [name] -- get the column name from parquet
+    -- [entity_id], 
+    JSON_VALUE(value, '$.entity_id') AS entity_id,
+    JSON_VALUE(value, '$.source_view_name') AS source_view_name,
+    JSON_VALUE(value, '$.dest_table_name') AS dest_table_name,
+    JSON_VALUE(value, '$.execution_order') AS execution_order,
+    JSON_VALUE(value, '$.schedule_recurrence') AS schedule_recurrence
+  FROM
+    OPENJSON(@entity, '$.edw.entities') e
+)
+
+,
+view_columns AS (
+  SELECT
+    e.entity_id,
+    e.source_view_name,
+    e.dest_table_name,
+    vc.name AS view_column_name
+  FROM
+    entity AS e
+  LEFT JOIN
+    sys.objects v
+    ON
+      v.name = e.source_view_name
+  LEFT JOIN
+    sys.columns AS vc
+    ON
+      vc.object_id = v.object_id
+)
+,
+table_columns AS (
+  SELECT
+    e.entity_id,
+    e.source_view_name,
+    e.dest_table_name,
+    tc.name AS table_column_name
+  FROM
+    entity AS e
+  LEFT JOIN
+    sys.objects t
+    ON
+      t.name = e.dest_table_name
+  LEFT JOIN
+    sys.columns AS tc
+    ON
+      tc.object_id = t.object_id
+)
+
+SELECT
+  vc.entity_id,
+  vc.source_view_name,
+  vc.dest_table_name,
+  vc.view_column_name,
+  tc.table_column_name
+FROM 
+  view_columns vc
+LEFT JOIN
+  table_columns tc
+  ON
+    vc.entity_id = tc.entity_id
+    AND
+    vc.view_column_name = tc.table_column_name
+WHERE
+    tc.table_column_name IS NULL
+    AND
+    vc.view_column_name IS NOT NULL
+  -- )
+  -- TODO exclude table columns with AUTO INCREMENT
+
+UNION ALL
+
+SELECT
+  tc.entity_id,
+  tc.source_view_name,
+  tc.dest_table_name,
+  vc.view_column_name,
+  tc.table_column_name
+FROM 
+  table_columns tc
+LEFT JOIN
+   view_columns vc
+  ON
+    vc.entity_id = tc.entity_id
+    AND
+    vc.view_column_name = tc.table_column_name
+WHERE
+    tc.table_column_name NOT IN ('t_jobId', 't_lastActionCd', 't_jobBy', 't_jobDtm')
+    AND
+    vc.view_column_name IS NULL
+    AND
+    tc.table_column_name IS NOT NULL
